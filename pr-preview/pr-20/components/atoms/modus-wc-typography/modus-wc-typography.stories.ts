55c71bb7b7d8a787774a4313bc3cb882a63b7ca1
gabriel-piltzer-trimble <gabriel-piltzer-trimble@users.noreply.github.com>
2024-10-14 23:23:11 +0000

    Deploy preview for PR 20 🛫

---
+++ pr-preview/pr-20/components/atoms/modus-wc-typography/modus-wc-typography.stories.ts
@@ -0,0 +1,81 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import { setModusWCMode, ModusWCMode } from '../../../utils/theme';
+import { TypographyBodySize, TypographyVariant } from './modus-wc-typography';
+
+interface TypographyArgs {
+  ariaLabel: string;
+  bodySize: TypographyBodySize;
+  customClass: string;
+  variant: TypographyVariant;
+  weight: 'regular' | 'semibold' | 'bold';
+  textCase: 'sentence' | 'title' | 'uppercase';
+  content: string;
+  mode: ModusWCMode;
+}
+
+const meta: Meta<TypographyArgs> = {
+  title: 'Components/Typography',
+  argTypes: {
+    ariaLabel: { control: 'text' },
+    bodySize: {
+      control: { type: 'select' },
+      options: ['standard', 'small', 'mini'],
+    },
+    customClass: { control: 'text' },
+    variant: {
+      control: { type: 'select' },
+      options: ['body', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'],
+    },
+    weight: {
+      control: { type: 'select' },
+      options: ['regular', 'semibold', 'bold'],
+    },
+    textCase: {
+      control: { type: 'select' },
+      options: ['sentence', 'title', 'uppercase'],
+    },
+    content: { control: 'text' },
+    mode: {
+      control: { type: 'select' },
+      options: ['default', 'dark', 'high-contrast'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<TypographyArgs>;
+
+const Template: Story = {
+  render: (args) => {
+    setModusWCMode(args.mode);
+
+    return `
+      <div>
+        <h1>Typography</h1>
+        <modus-wc-typography 
+          aria-label="${args.ariaLabel}"
+          body-size="${args.bodySize}"
+          custom-class="${args.customClass}"
+          variant="${args.variant}"
+          weight="${args.weight}"
+          text-case="${args.textCase}"
+        >${args.content}</modus-wc-typography>
+        <stencil-docs component-name="modus-wc-typography"></stencil-docs>
+      </div>
+    `;
+  },
+};
+
+export const Default: Story = {
+  ...Template,
+  args: {
+    ariaLabel: 'Example typography',
+    content: 'The quick brown fox jumps over the lazy dog',
+    customClass: '',
+    mode: 'default',
+    textCase: 'sentence',
+    variant: 'p',
+    weight: 'regular',
+  },
+};
