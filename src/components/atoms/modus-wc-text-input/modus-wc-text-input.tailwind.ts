1afac4bd6703debbc42449c07f6ba46a577b11ff
Gabe Piltzer <gabriel_piltzer@trimble.com>
2024-10-22 11:33:09 -0700

    feat: add text input props to set tailwind classes

--- src/components/atoms/modus-wc-text-input/modus-wc-text-input.tailwind.ts
+++ src/components/atoms/modus-wc-text-input/modus-wc-text-input.tailwind.ts
@@ -1,5 +1,18 @@
 export const tailwindThemeClasses: Record<string, string> = {
-  'modus-classic':
-    'modus-wc-input-bordered modus-wc-input-primary modus-wc-w-full modus-wc-w-xs',
+  'modus-classic': 'modus-wc-input-bordered modus-wc-w-full modus-wc-w-xs',
   'modus-dark': '',
+};
+
+export const convertPropsToClasses = (props: any): string => {
+  let classes = '';
+
+  if (props.hasOwnProperty('bordered') && !!props.bordered) {
+    classes = `${classes} modus-wc-input-bordered`;
+  }
+
+  if (props.hasOwnProperty('size') && !!props.size) {
+    classes = `${classes} ${props.size}`;
+  }
+
+  return classes;
 };
