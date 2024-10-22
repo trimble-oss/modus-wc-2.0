adf9cd51d1e5a529bbffb153a4f3d9c7766ccff7
Gabe Piltzer <gabriel_piltzer@trimble.com>
2024-10-22 09:49:35 -0700

    feat: custom themes working

---
+++ src/components/atoms/modus-wc-text-input/modus-wc-text-input.tailwind.ts
@@ -0,0 +1,5 @@
+export const tailwindThemeClasses: Record<string, string> = {
+  'modus-classic':
+    'modus-wc-input-bordered modus-wc-input-primary modus-wc-w-full modus-wc-w-xs',
+  'modus-dark': '',
+};
--- src/styles/themes/modus-dark.ts
+++ src/styles/themes/modus-dark.ts
@@ -1,44 +1,44 @@
-export const modusClassic = {
+export const modusDark = {
   // Primary Colors
-  primary: '#0063a3',
-  'primary-focus': '#003054',
+  primary: '#368fc7',
+  'primary-focus': '#217cbb',
   'primary-content': '#ffffff',
 
   // Secondary Colors
-  secondary: '#fcb647',
-  'secondary-focus': '#d88228',
-  'secondary-content': '#ffffff',
+  secondary: '#fec157',
+  'secondary-focus': '#fcb647',
+  'secondary-content': '#000000',
 
   // Tertiary/Accent Colors
-  accent: '#95301f',
-  'accent-focus': '#662a12',
+  accent: '#b44e2a',
+  'accent-focus': '#95301f',
   'accent-content': '#ffffff',
 
   // Neutral Colors
-  neutral: '#464b52',
-  'neutral-focus': '#252a2e',
+  neutral: '#7d808d',
+  'neutral-focus': '#464b52',
   'neutral-content': '#ffffff',
 
   // Base Colors
-  'base-100': '#ffffff',
-  'base-200': '#e0e1e9',
-  'base-300': '#b7b9c3',
-  'base-content': '#171c1e',
+  'base-100': '#252a2e',
+  'base-200': '#171c1e',
+  'base-300': '#000000',
+  'base-content': '#e0e1e9',
 
   // State/Feedback Colors
-  info: '#217cbb',
-  'info-content': '#ffffff',
-  success: '#1e8a44',
+  info: '#6cb7e2',
+  'info-content': '#000000',
+  success: '#4ea646',
   'success-content': '#ffffff',
-  warning: '#fbad26',
+  warning: '#fec157',
   'warning-content': '#000000',
-  error: '#da212c',
+  error: '#e86363',
   'error-content': '#ffffff',
 
   // Design Tokens
   '--rounded-box': '0.5rem',
-  '--rounded-btn': '0.25rem', // 4px
-  '--rounded-badge': '0.25rem', // 4px
+  '--rounded-btn': '0.25rem',
+  '--rounded-badge': '0.25rem',
 
   // Animation
   '--animation-btn': '0.3s',
