9173bad0b73d5ed7448dd00405bfaeab63994c5b
Kai-Richardson <Kai-Richardson@users.noreply.github.com>
2024-11-05 18:12:18 +0000

    Deploy preview for PR 44 🛫

---
+++ pr-preview/pr-44/assets/modus-wc-divider.stories-B-RG4oRZ.js
@@ -0,0 +1,14 @@
+import{k as s}from"./lit-element-DVRzCIa_.js";const i={title:"Components/Atoms/Divider",component:"modus-wc-divider"},n={render:e=>s`
+      <modus-wc-divider
+        aria-label="${e["aria-label"]}"
+        ?custom-class="${e["custom-class"]}"
+        ?daisy-class="${e["daisy-class"]}"
+        content="${e.content}"
+      ></modus-wc-divider>
+    `},a={...n,args:{"aria-label":"Horizontal divider",content:""}};var t,o,r;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    'aria-label': 'Horizontal divider',
+    content: ''
+  }
+}`,...(r=(o=a.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const d=["Default"];export{a as Default,d as __namedExportsOrder,i as default};
