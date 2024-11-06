b215c83ae594e74ffdb36a50aa7dba845f58a19a
Kai-Richardson <Kai-Richardson@users.noreply.github.com>
2024-11-06 17:52:05 +0000

    Deploy preview for PR 46 🛫

--- pr-preview/pr-46/assets/modus-wc-textarea.stories-0ZXp46xK.js
+++ pr-preview/pr-46/assets/modus-wc-textarea.stories-0ZXp46xK.js
@@ -1,4 +1,4 @@
-import{w as i}from"./decorator-Dt3Huotz.js";import{k as n}from"./lit-element-DVRzCIa_.js";import{t as a}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const x={title:"Components/Atoms/Textarea",component:"modus-wc-textarea",argTypes:{"textarea-aria-invalid":{control:{type:"inline-radio"},options:["grammar","spelling","true","false"]},"textarea-dir":{control:{type:"inline-radio"},options:["ltr","rtl","auto"]}},decorators:[i],parameters:{actions:{handles:["textareaBlur","textareaChange","textareaFocus"]}}},d={render:e=>n`
+import{w as i}from"./decorator-Dt3Huotz.js";import{k as n}from"./lit-element-DVRzCIa_.js";import{t as a}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const x={title:"Components/Atoms/Textarea",component:"modus-wc-textarea",args:{"aria-label":"Enter your comments",placeholder:"Type your comments here"},argTypes:{"textarea-aria-invalid":{control:{type:"inline-radio"},options:["grammar","spelling","true","false"]},"textarea-dir":{control:{type:"inline-radio"},options:["ltr","rtl","auto"]}},decorators:[i],parameters:{actions:{handles:["textareaBlur","textareaChange","textareaFocus"]}}},d={render:e=>n`
       <modus-wc-textarea
         aria-describedby=${a(e["aria-describedby"])}
         aria-label=${a(e["aria-label"])}
@@ -17,6 +17,6 @@ import{w as i}from"./decorator-Dt3Huotz.js";import{k as n}from"./lit-element-DVR
         ?textarea-tab-index=${e["textarea-tab-index"]}
         .value=${e.value}
       ></modus-wc-textarea>
-    `,args:{"aria-label":"Enter your comments",placeholder:"Type your comments here"}},t={...d};var r,o,l;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+    `},t={...d};var r,o,l;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
   ...Template
 }`,...(l=(o=t.parameters)==null?void 0:o.docs)==null?void 0:l.source}}};const u=["Default"];export{t as Default,u as __namedExportsOrder,x as default};
