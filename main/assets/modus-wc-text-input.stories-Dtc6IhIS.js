fc85d7d130a03cb0536364468fc11df3c2144de8
gabriel-piltzer-trimble <83187108+gabriel-piltzer-trimble@users.noreply.github.com>
2024-11-06 17:17:46 +0000

    Deploying to gh-pages from @ Trimble-Construction/poc-modus-wc-2.0@6411ad150c0e92b0ec17e2760c9fe7a4d10b0f5d 🚀

--- main/assets/modus-wc-text-input.stories-Dtc6IhIS.js
+++ main/assets/modus-wc-text-input.stories-Dtc6IhIS.js
@@ -1,4 +1,4 @@
-import{k as p}from"./lit-element-DVRzCIa_.js";import{t as u}from"./if-defined-1ipA9LQg.js";const m={title:"Components/Atoms/Text Input",component:"modus-wc-text-input",tags:["autodocs"],argTypes:{"auto-capitalize":{control:{type:"select"},options:["off","none","on","sentences","words","characters"]},"auto-complete":{control:{type:"select"},options:["on","off"]},"input-dir":{control:{type:"select"},options:["ltr","rtl","auto"]},"input-mode":{control:{type:"select"},options:["decimal","email","none","numeric","search","tel","text","url"]},size:{control:{type:"select"},options:["sm","md","lg"]},type:{control:{type:"select"},options:["email","password","search","tel","text","url"]}}},c={render:e=>p`
+import{k as p}from"./lit-element-DVRzCIa_.js";import{t as u}from"./if-defined-1ipA9LQg.js";const m={title:"Components/Atoms/Text Input",component:"modus-wc-text-input",tags:["autodocs"],args:{"aria-label":"Enter your name","input-mode":"text",name:"",placeholder:"Type your name here",size:"md",type:"text",value:""},argTypes:{"auto-capitalize":{control:{type:"select"},options:["off","none","on","sentences","words","characters"]},"auto-complete":{control:{type:"select"},options:["on","off"]},"input-dir":{control:{type:"select"},options:["ltr","rtl","auto"]},"input-mode":{control:{type:"select"},options:["decimal","email","none","numeric","search","tel","text","url"]},size:{control:{type:"select"},options:["sm","md","lg"]},type:{control:{type:"select"},options:["email","password","search","tel","text","url"]}}},c={render:e=>p`
     <modus-wc-text-input
       aria-describedby=${u(e["aria-describedby"])}
       aria-label=${e["aria-label"]}
@@ -28,6 +28,6 @@ import{k as p}from"./lit-element-DVRzCIa_.js";import{t as u}from"./if-defined-1i
       @inputChange=${t=>{const o=t.target;e.value=o.value}}
       @inputFocus=${t=>{const o=t.target;e.value=o.value}}
     ></modus-wc-text-input>
-  `,args:{"aria-label":"Enter your name","input-mode":"text",name:"",placeholder:"Type your name here",size:"md",type:"text",value:""}},n={...c};var a,l,i;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  `},n={...c};var a,l,i;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
   ...Template
 }`,...(i=(l=n.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const s=["Default"];export{n as Default,s as __namedExportsOrder,m as default};
--- main/assets/modus-wc-textarea.stories-DYqV08Du.js
+++ main/assets/modus-wc-textarea.stories-DYqV08Du.js
@@ -1,4 +1,4 @@
-import{k as d}from"./lit-element-DVRzCIa_.js";import{t as a}from"./if-defined-1ipA9LQg.js";const u={title:"Components/Atoms/Textarea",component:"modus-wc-textarea",argTypes:{"textarea-aria-invalid":{control:{type:"inline-radio"},options:["grammar","spelling","true","false"]},"textarea-dir":{control:{type:"inline-radio"},options:["ltr","rtl","auto"]}}},c={render:e=>d`
+import{k as d}from"./lit-element-DVRzCIa_.js";import{t as a}from"./if-defined-1ipA9LQg.js";const u={title:"Components/Atoms/Textarea",component:"modus-wc-textarea",args:{"aria-label":"Enter your comments",placeholder:"Type your comments here"},argTypes:{"textarea-aria-invalid":{control:{type:"inline-radio"},options:["grammar","spelling","true","false"]},"textarea-dir":{control:{type:"inline-radio"},options:["ltr","rtl","auto"]}}},c={render:e=>d`
       <modus-wc-textarea
         aria-describedby=${a(e["aria-describedby"])}
         aria-label=${a(e["aria-label"])}
@@ -20,6 +20,6 @@ import{k as d}from"./lit-element-DVRzCIa_.js";import{t as a}from"./if-defined-1i
         @textarea-change=${t=>{const r=t.target;e.value=r.value}}
         @textarea-focus=${t=>{const r=t.target;e.value=r.value}}
       ></modus-wc-textarea>
-    `,args:{"aria-label":"Enter your comments",placeholder:"Type your comments here"}},o={...c};var l,i,n;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+    `},o={...c};var l,i,n;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
   ...Template
 }`,...(n=(i=o.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const x=["Default"];export{o as Default,x as __namedExportsOrder,u as default};
