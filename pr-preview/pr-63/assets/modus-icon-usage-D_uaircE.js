922354c1fa897b7919441b64c21896f2345d32dc
Kai-Richardson <Kai-Richardson@users.noreply.github.com>
2024-11-13 00:30:04 +0000

    Deploy preview for PR 63 🛫

---
+++ pr-preview/pr-63/assets/modus-icon-usage-D_uaircE.js
@@ -0,0 +1,15 @@
+import{ae as e,af as r}from"./index-DS8evEbC.js";import{useMDXComponents as t}from"./index-BSj771as.js";import"./iframe-B_5Is18N.js";import"../sb-preview/runtime.js";import"./index-B-o1Wr-g.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";function s(n){const o={a:"a",code:"code",h1:"h1",hr:"hr",p:"p",pre:"pre",...t(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Documentation/Modus Icon Usage"}),`
+`,e.jsx(o.h1,{id:"modus-icon-usage",children:"Modus Icon Usage"}),`
+`,e.jsx(o.p,{children:`Several components require Modus Icons to be installed in the host application. To install
+icons, add the following html to your application.`}),`
+`,e.jsxs(o.p,{children:["Be sure to reference the latest version of ",e.jsx(o.code,{children:"@trimble-oss/modus-icons"})]}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-html",children:`<head>
+  <link
+    href="https://cdn.jsdelivr.net/npm/@trimble-oss/modus-icons@latest/dist/modus-outlined/fonts/modus-icons.css"
+    rel="stylesheet"
+  />
+</head>
+`})}),`
+`,e.jsx(o.hr,{}),`
+`,e.jsxs(o.p,{children:["Need help? Check out our ",e.jsx(o.a,{href:"https://github.com/trimble-oss/modus-web-components",rel:"nofollow",children:"GitHub repository"}),`
+or `,e.jsx(o.a,{href:"https://github.com/trimble-oss/modus-web-components/issues",rel:"nofollow",children:"raise an issue"}),"."]})]})}function x(n={}){const{wrapper:o}={...t(),...n.components};return o?e.jsx(o,{...n,children:e.jsx(s,{...n})}):s(n)}export{x as default};
