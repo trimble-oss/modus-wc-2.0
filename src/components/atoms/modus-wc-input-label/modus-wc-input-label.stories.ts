ae240c8a6d78cb9629200d27614d84b93e18f482
Gabriel Piltzer <83187108+gabriel-piltzer-trimble@users.noreply.github.com>
2024-11-14 19:07:41 -0800

    feat: add input label component (#69)

---
+++ src/components/atoms/modus-wc-input-label/modus-wc-input-label.stories.ts
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+interface InputLabelArgs {
+  'for-id'?: string;
+  'custom-class'?: string;
+  'label-dir'?: '' | 'ltr' | 'rtl' | 'auto';
+  'label-text'?: string;
+  required?: boolean;
+}
+
+const meta: Meta<InputLabelArgs> = {
+  title: 'Components/Atoms/Input Label',
+  component: 'modus-wc-input-label',
+  args: {
+    'label-text': 'Label',
+    required: false,
+  },
+  argTypes: {
+    'label-dir': {
+      control: { type: 'inline-radio' },
+      options: ['ltr', 'rtl', 'auto'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<InputLabelArgs>;
+
+const Template: Story = {
+  render: (args) => html`
+    <modus-wc-input-label
+      for-id="${ifDefined(args['for-id'])}"
+      custom-class="${ifDefined(args['custom-class'])}"
+      label-dir="${ifDefined(args['label-dir'])}"
+      label-text="${ifDefined(args['label-text'])}"
+      ?required="${args['required']}"
+    ></modus-wc-input-label>
+  `,
+};
+
+export const Default: Story = { ...Template };
