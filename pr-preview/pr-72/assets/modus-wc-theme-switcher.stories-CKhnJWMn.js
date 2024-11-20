ed6b7f526ad85b445a575f2158ecf0b901725abc
Kai-Richardson <Kai-Richardson@users.noreply.github.com>
2024-11-20 22:02:15 +0000

    Deploy preview for PR 72 🛫

---
+++ pr-preview/pr-72/assets/modus-wc-theme-switcher.stories-CKhnJWMn.js
@@ -0,0 +1,10 @@
+import{w as r}from"./decorator-Dt3Huotz.js";import{k as i}from"./lit-element-DVRzCIa_.js";import{t as m}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const p={title:"Components/Molecules/ThemeSwitcher",component:"modus-wc-theme-switcher",args:{"aria-label":"Toggle theme","custom-class":void 0,"initial-theme":void 0},argTypes:{"aria-label":{control:"text",description:"The aria-label attribute for accessibility."},"custom-class":{control:"text",description:"Custom CSS class to apply to the theme switcher element."},"initial-theme":{control:"object",description:"Initial theme configuration (set on the theme provider).",table:{defaultValue:{summary:'{ theme: "modus-classic" }'}}}},decorators:[r],parameters:{actions:{handles:["themeChange"]}}},c={render:t=>i`
+    <modus-wc-theme-provider .initialTheme=${t["initial-theme"]}>
+      <modus-wc-theme-switcher
+        aria-label=${t["aria-label"]}
+        custom-class=${m(t["custom-class"])}
+      ></modus-wc-theme-switcher>
+    </modus-wc-theme-provider>
+  `},e={...c};var o,a,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...Template
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,p as default};
