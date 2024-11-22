8feb1ea1ad1cc7e55e2612d9493b711a06167211
Kai Richardson <kai_richardson@trimble.com>
2024-11-22 10:33:39 -0800

    React v18 Test Project (#72)

---
+++ integrations/react/test-react-v18/vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		chunkSizeWarningLimit: 600,
+		rollupOptions: {
+			output: {
+				manualChunks(id){
+					if(id.includes('stencil-generated')){
+						return 'modus-wc';
+					}
+					else if(id.includes('node_modules')){
+						return 'vendor';
+					}
+				}
+			}
+		}
+	}
+});
