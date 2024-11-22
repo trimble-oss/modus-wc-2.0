abbfc17fd2dda1e3edf3fc3a21adeb7de54bf869
gabriel-piltzer-trimble <gabriel-piltzer-trimble@users.noreply.github.com>
2024-11-22 23:58:54 +0000

    Deploy preview for PR 76 🛫

--- pr-preview/pr-76/assets/modus-wc-number-input.stories-D_3IfIRC.js
+++ pr-preview/pr-76/assets/modus-wc-number-input.stories-D_3IfIRC.js
@@ -1,11 +1,11 @@
-import{w as p}from"./decorator-Dt3Huotz.js";import{k as m}from"./lit-element-DVRzCIa_.js";import{t as n}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const $={title:"Components/Atoms/Number Input",component:"modus-wc-number-input",args:{"aria-label":"Enter your age",bordered:!0,disabled:!1,"input-mode":"numeric",placeholder:"Type your age here",size:"md",type:"number",value:""},argTypes:{"auto-complete":{control:{type:"inline-radio"},options:["on","off"]},"input-aria-invalid":{control:{type:"inline-radio"},options:["true","false"]},"input-dir":{control:{type:"inline-radio"},options:["ltr","rtl","auto"]},"input-mode":{control:{type:"inline-radio"},options:["decimal","none","numeric"]},size:{control:{type:"inline-radio"},options:["sm","md","lg"]},type:{control:{type:"inline-radio"},options:["number","range"]}},decorators:[p],parameters:{actions:{handles:["inputBlur","inputChange","inputFocus"]}}},i={render:e=>m`
+import{w as m}from"./decorator-Dt3Huotz.js";import{k as l}from"./lit-element-DVRzCIa_.js";import{t as n}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const $={title:"Components/Atoms/Number Input",component:"modus-wc-number-input",args:{"aria-label":"Enter your age",bordered:!0,disabled:!1,"input-mode":"numeric",placeholder:"Type your age here",size:"md",type:"number",value:""},argTypes:{"auto-complete":{control:{type:"inline-radio"},options:["on","off"]},"input-aria-invalid":{control:{type:"inline-radio"},options:["true","false"]},"input-dir":{control:{type:"inline-radio"},options:["ltr","rtl","auto"]},"input-mode":{control:{type:"inline-radio"},options:["decimal","none","numeric"]},size:{control:{type:"inline-radio"},options:["sm","md","lg"]},type:{control:{type:"inline-radio"},options:["number","range"]}},decorators:[m],parameters:{actions:{handles:["inputBlur","inputChange","inputFocus"]}}},i={render:e=>l`
     <modus-wc-number-input
       aria-describedby=${n(e["aria-describedby"])}
       aria-label=${e["aria-label"]}
       auto-complete=${n(e["auto-complete"])}
       ?auto-focus=${e["auto-focus"]}
       ?bordered=${e.bordered}
-      custom-class=${e["custom-class"]}
+      custom-class=${n(e["custom-class"])}
       ?disabled=${e.disabled}
       input-aria-invalid=${n(e["input-aria-invalid"])}
       input-dir=${n(e["input-dir"])}
@@ -15,14 +15,15 @@ import{w as p}from"./decorator-Dt3Huotz.js";import{k as m}from"./lit-element-DVR
       max=${n(e.max)}
       min=${n(e.min)}
       name=${n(e.name)}
-      placeholder=${e.placeholder}
+      placeholder=${n(e.placeholder)}
       ?read-only=${e["read-only"]}
       ?required=${e.required}
-      size=${e.size}
-      type=${e.type}
+      size=${n(e.size)}
+      step=${e.step}
+      type=${n(e.type)}
       .value=${e.value}
     ></modus-wc-number-input>
-  `},a={render:()=>m`
+  `},a={render:()=>l`
       <form action="" class="form-example" method="get">
         <div class="form-example">
           <modus-wc-input-label
@@ -45,7 +46,7 @@ import{w as p}from"./decorator-Dt3Huotz.js";import{k as m}from"./lit-element-DVR
           padding-inline-end: 8px;
         }
       </style>
-    `};var r,t,o;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:`{
+    `};var t,r,o;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`{
   render: args => html\`
     <modus-wc-number-input
       aria-describedby=\${ifDefined(args['aria-describedby'])}
@@ -53,7 +54,7 @@ import{w as p}from"./decorator-Dt3Huotz.js";import{k as m}from"./lit-element-DVR
       auto-complete=\${ifDefined(args['auto-complete'])}
       ?auto-focus=\${args['auto-focus']}
       ?bordered=\${args.bordered}
-      custom-class=\${args['custom-class']}
+      custom-class=\${ifDefined(args['custom-class'])}
       ?disabled=\${args.disabled}
       input-aria-invalid=\${ifDefined(args['input-aria-invalid'])}
       input-dir=\${ifDefined(args['input-dir'])}
@@ -63,15 +64,16 @@ import{w as p}from"./decorator-Dt3Huotz.js";import{k as m}from"./lit-element-DVR
       max=\${ifDefined(args.max)}
       min=\${ifDefined(args.min)}
       name=\${ifDefined(args.name)}
-      placeholder=\${args.placeholder}
+      placeholder=\${ifDefined(args.placeholder)}
       ?read-only=\${args['read-only']}
       ?required=\${args.required}
-      size=\${args.size}
-      type=\${args.type}
+      size=\${ifDefined(args.size)}
+      step=\${args.step}
+      type=\${ifDefined(args.type)}
       .value=\${args.value}
     ></modus-wc-number-input>
   \`
-}`,...(o=(t=i.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var u,d,l;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+}`,...(o=(r=i.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};var u,d,p;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
   render: () => {
     return html\`
       <form action="" class="form-example" method="get">
@@ -98,4 +100,4 @@ import{w as p}from"./decorator-Dt3Huotz.js";import{k as m}from"./lit-element-DVR
       </style>
     \`;
   }
-}`,...(l=(d=a.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const y=["Template","NumberInputWithLabel"];export{a as NumberInputWithLabel,i as Template,y as __namedExportsOrder,$ as default};
+}`,...(p=(d=a.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const y=["Template","NumberInputWithLabel"];export{a as NumberInputWithLabel,i as Template,y as __namedExportsOrder,$ as default};
