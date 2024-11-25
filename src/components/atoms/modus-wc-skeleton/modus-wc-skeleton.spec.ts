fdb6b17e5b2d5688e96de784a2e6c4410f67a27f
Stuart Rimel <stuart_rimel@trimble.com>
2024-11-25 15:40:24 -0800

    update unit tests

--- src/components/atoms/modus-wc-skeleton/modus-wc-skeleton.spec.ts
+++ src/components/atoms/modus-wc-skeleton/modus-wc-skeleton.spec.ts
@@ -2,25 +2,10 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ModusWcSkeleton } from './modus-wc-skeleton';
 
 describe('modus-wc-skeleton', () => {
-  it('should warn if aria-label is not provided', async () => {
-    const consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation();
-
-    await newSpecPage({
-      components: [ModusWcSkeleton],
-      html: '<modus-wc-skeleton></modus-wc-skeleton>',
-    });
-
-    expect(consoleWarnSpy).toHaveBeenCalledWith(
-      'ModusWcSkeleton: aria-label is required for accessibility.'
-    );
-
-    consoleWarnSpy.mockRestore();
-  });
-
   it('renders with default props', async () => {
     const page = await newSpecPage({
       components: [ModusWcSkeleton],
-      html: '<modus-wc-skeleton aria-label="Default skeleton"></modus-wc-skeleton>',
+      html: '<modus-wc-skeleton></modus-wc-skeleton>',
     });
     expect(page.root).toMatchSnapshot();
   });
@@ -29,12 +14,13 @@ describe('modus-wc-skeleton', () => {
     const page = await newSpecPage({
       components: [ModusWcSkeleton],
       html: `<modus-wc-skeleton
-            aria-describedby="active"
-            aria-label="Test skeleton"
-            aria-labelledby="skeleton-label"
-            custom-class="test-class
+            aria-hidden="false"
+            height="5rem"
+            role="status"
+            custom-class="test-class"
             shape="circle"
-            full-width="false"
+            tabindex="1"
+            width="5rem"
             ></modus-wc-skeleton>`,
     });
     expect(page.root).toMatchSnapshot();
