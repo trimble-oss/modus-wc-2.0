b8966512e9be37acaecaf4d4a84347e750c22740
gabriel-piltzer-trimble <83187108+gabriel-piltzer-trimble@users.noreply.github.com>
2024-11-26 23:23:06 +0000

    Deploying to gh-pages from @ Trimble-Construction/modus-wc-2.0@fd55271614db927b8f115b24e069a266e5276c60 🚀

---
+++ main/assets/modus-wc-avatar.stories-CzgSgw5D.js
@@ -0,0 +1,12 @@
+import{k as o}from"./lit-element-DVRzCIa_.js";const i={title:"Components/Avatar",component:"modus-wc-avatar",args:{alt:"Example avatar","aria-label":"Example avatar","img-src":"https://i.pinimg.com/474x/73/54/79/7354794bf3873c3ef2666f778da4bcac.jpg",shape:"circle",size:"md"},argTypes:{shape:{control:{type:"inline-radio"},options:["circle","square"]},size:{control:{type:"inline-radio"},options:["xs","sm","md","lg","xl"]}}},c={render:a=>o`
+      <modus-wc-avatar
+        alt="${a.alt}"
+        aria-label="${a["aria-label"]}"
+        ?custom-class="${a["custom-class"]}"
+        img-src="${a["img-src"]}"
+        shape="${a.shape}"
+        size="${a.size}"
+      ></modus-wc-avatar>
+    `},e={...c};var t,s,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  ...Template
+}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,i as default};
