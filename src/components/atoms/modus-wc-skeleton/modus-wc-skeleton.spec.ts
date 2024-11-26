f618479d07d8a9bf3c4bf901b9c17e6b8d794d14
Stuart Rimel <stuart_rimel@trimble.com>
2024-11-26 14:09:09 -0800

    alphabetize props in skeleton unit test case

--- src/components/atoms/modus-wc-skeleton/modus-wc-skeleton.spec.ts
+++ src/components/atoms/modus-wc-skeleton/modus-wc-skeleton.spec.ts
@@ -15,9 +15,9 @@ describe('modus-wc-skeleton', () => {
       components: [ModusWcSkeleton],
       html: `<modus-wc-skeleton
             aria-hidden="false"
+            custom-class="test-class"
             height="5rem"
             role="status"
-            custom-class="test-class"
             shape="circle"
             tabindex="1"
             width="5rem"
