46d28a00987243ffddfbff592338c8632245e9fd
Gabe Piltzer <gabriel_piltzer@trimble.com>
2024-12-06 15:40:36 -0800

    chore: update angular bindings

--- angular-value-accessor-bindings.ts
+++ angular-value-accessor-bindings.ts
@@ -2,43 +2,19 @@ import { ValueAccessorConfig } from '@stencil/angular-output-target';
 
 const angularValueAccessorBindings: ValueAccessorConfig[] = [
   {
-    elementSelectors: ['modus-wc-checkbox'],
+    elementSelectors: ['modus-wc-checkbox', 'modus-wc-radio'],
     event: 'inputChange',
     targetAttr: 'value',
     type: 'boolean',
   },
   {
-    elementSelectors: ['modus-wc-date'],
-    event: 'inputChange',
-    targetAttr: 'value',
-    type: 'text',
-  },
-  {
-    elementSelectors: ['modus-wc-number-input'],
-    event: 'inputChange',
-    targetAttr: 'value',
-    type: 'text',
-  },
-  {
-    elementSelectors: ['modus-wc-radio'],
-    event: 'inputChange',
-    targetAttr: 'value',
-    type: 'boolean',
-  },
-  {
-    elementSelectors: ['modus-wc-select'],
-    event: 'inputChange',
-    targetAttr: 'value',
-    type: 'text',
-  },
-  {
-    elementSelectors: ['modus-wc-text-input'],
-    event: 'inputChange',
-    targetAttr: 'value',
-    type: 'text',
-  },
-  {
-    elementSelectors: ['modus-wc-textarea'],
+    elementSelectors: [
+      'modus-wc-date',
+      'modus-wc-number-input',
+      'modus-wc-select',
+      'modus-wc-text-input',
+      'modus-wc-textarea',
+    ],
     event: 'inputChange',
     targetAttr: 'value',
     type: 'text',
