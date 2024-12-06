1c5f4bf44162ef352367eb4864d39f52057358ed
stuart-rimel <stuart-rimel@users.noreply.github.com>
2024-12-06 23:13:23 +0000

    Deploy preview for PR 84 🛫

---
+++ pr-preview/pr-84/assets/modus-wc-loader.stories-DusN2JmF.js
@@ -0,0 +1,141 @@
+import{k as a}from"./lit-element-DVRzCIa_.js";import{t as R}from"./if-defined-1ipA9LQg.js";const E={title:"Components/Loader",component:"modus-wc-loader",args:{"aria-label":"Loading spinner",color:"primary","custom-class":"",size:"md",variant:"spinner"},argTypes:{color:{control:{type:"inline-radio"},options:["primary","secondary","tertiary"]},size:{control:{type:"inline-radio"},options:["xs","sm","md","lg","xl"]},variant:{control:{type:"inline-radio"},options:["ball","bars","dots","infinity","ring","spinner"]}}},s={render:r=>a`
+      <modus-wc-loader
+        aria-label="${R(r["aria-label"])}"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="${r.size}"
+        variant="${r.variant}"
+      ></modus-wc-loader>
+    `},o={render:r=>a`
+      <modus-wc-loader
+        aria-label="Loading ball"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="md"
+        variant="ball"
+      ></modus-wc-loader>
+    `},n={render:r=>a`
+      <modus-wc-loader
+        aria-label="Loading bars"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="md"
+        variant="bars"
+      ></modus-wc-loader>
+    `},e={render:r=>a`
+      <modus-wc-loader
+        aria-label="Loading dots"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="md"
+        variant="dots"
+      ></modus-wc-loader>
+    `},c={render:r=>a`
+      <modus-wc-loader
+        aria-label="Loading infinity symbol"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="md"
+        variant="infinity"
+      ></modus-wc-loader>
+    `},l={render:r=>a`
+      <modus-wc-loader
+        aria-label="Loading ring"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="md"
+        variant="ring"
+      ></modus-wc-loader>
+    `},t={render:r=>a`
+      <modus-wc-loader
+        aria-label="Loading spinner"
+        color="${r.color}"
+        custom-class="${r["custom-class"]}"
+        size="md"
+        variant="spinner"
+      ></modus-wc-loader>
+    `};var i,d,m;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="\${ifDefined(args['aria-label'])}"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="\${args.size}"
+        variant="\${args.variant}"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(m=(d=s.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var u,p,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="Loading ball"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="md"
+        variant="ball"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(g=(p=o.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var $,b,w;n.parameters={...n.parameters,docs:{...($=n.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="Loading bars"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="md"
+        variant="bars"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(w=(b=n.parameters)==null?void 0:b.docs)==null?void 0:w.source}}};var v,z,y;e.parameters={...e.parameters,docs:{...(v=e.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="Loading dots"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="md"
+        variant="dots"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(y=(z=e.parameters)==null?void 0:z.docs)==null?void 0:y.source}}};var f,L,S;c.parameters={...c.parameters,docs:{...(f=c.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="Loading infinity symbol"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="md"
+        variant="infinity"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(S=(L=c.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var h,D,x;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="Loading ring"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="md"
+        variant="ring"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(x=(D=l.parameters)==null?void 0:D.docs)==null?void 0:x.source}}};var B,k,I;t.parameters={...t.parameters,docs:{...(B=t.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  render: args => {
+    return html\`
+      <modus-wc-loader
+        aria-label="Loading spinner"
+        color="\${args.color}"
+        custom-class="\${args['custom-class']}"
+        size="md"
+        variant="spinner"
+      ></modus-wc-loader>
+    \`;
+  }
+}`,...(I=(k=t.parameters)==null?void 0:k.docs)==null?void 0:I.source}}};const O=["Default","Ball","Bars","Dots","Infinity","Ring","Spinner"];export{o as Ball,n as Bars,s as Default,e as Dots,c as Infinity,l as Ring,t as Spinner,O as __namedExportsOrder,E as default};
