84a684e93136c57146238897478b29130299e1ca
stuart-rimel <stuart-rimel@users.noreply.github.com>
2025-01-06 18:45:52 +0000

    Deploy preview for PR 95 🛫

---
+++ pr-preview/pr-95/assets/modus-wc-button.stories-uQoUvCqc.js
@@ -0,0 +1,16 @@
+import{w as l}from"./decorator-Dt3Huotz.js";import{k as i}from"./lit-element-DVRzCIa_.js";import{t as s}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const u={title:"Components/Button",component:"modus-wc-button",args:{"aria-label":"Click me button",color:"primary",disabled:!1,"full-width":!1,label:"Click me",pressed:!1,size:"md",type:"button",variant:"filled"},argTypes:{color:{control:{type:"inline-radio"},options:["primary","secondary","tertiary","warning","danger"]},size:{control:{type:"inline-radio"},options:["sm","md","lg"]},type:{control:{type:"inline-radio"},options:["button","submit","reset"]},variant:{control:{type:"inline-radio"},options:["borderless","filled","outlined"]}},decorators:[l],parameters:{actions:{handles:["buttonClick"]}}},n={render:e=>i`
+      <modus-wc-button
+        aria-label="${e["aria-label"]}"
+        color="${e.color}"
+        custom-class="${s(e["custom-class"])}"
+        ?disabled="${e.disabled}"
+        ?full-width="${e["full-width"]}"
+        label="${e.label}"
+        ?pressed="${e.pressed}"
+        size="${e.size}"
+        type="${e.type}"
+        variant="${e.variant}"
+      ></modus-wc-button>
+    `},t={...n};var o,r,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...Template
+}`,...(a=(r=t.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const b=["Default"];export{t as Default,b as __namedExportsOrder,u as default};
