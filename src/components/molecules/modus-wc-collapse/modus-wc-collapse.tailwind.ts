a0e4053ccca795e430c0cb90ab42e4044c01c390
Stuart Rimel <stuart_rimel@trimble.com>
2025-01-15 15:52:30 -0800

    remove comments

--- src/components/molecules/modus-wc-collapse/modus-wc-collapse.tailwind.ts
+++ src/components/molecules/modus-wc-collapse/modus-wc-collapse.tailwind.ts
@@ -69,16 +69,16 @@ export const convertCollapseSizeToTitleCollapsePadding = (props: {
   if (Object.prototype.hasOwnProperty.call(props, 'size') && props.size) {
     switch (props.size) {
       case 'xs':
-        classes = 'modus-wc-pb-2 modus-wc-pl-2 modus-wc-pt-2'; // .5rem
+        classes = 'modus-wc-pb-2 modus-wc-pl-2 modus-wc-pt-2';
         break;
       case 'sm':
-        classes = 'modus-wc-pb-3 modus-wc-pl-3 modus-wc-pt-3'; // .75rem
+        classes = 'modus-wc-pb-3 modus-wc-pl-3 modus-wc-pt-3';
         break;
       case 'md':
-        classes = 'modus-wc-pb-4 modus-wc-pl-4 modus-wc-pt-4'; // 1rem
+        classes = 'modus-wc-pb-4 modus-wc-pl-4 modus-wc-pt-4';
         break;
       case 'lg':
-        classes = 'modus-wc-pb-5 modus-wc-pl-5 modus-wc-pt-5'; // 1.25rem
+        classes = 'modus-wc-pb-5 modus-wc-pl-5 modus-wc-pt-5';
         break;
     }
   }
