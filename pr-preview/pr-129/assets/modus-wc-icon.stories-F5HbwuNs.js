0a30d6aacc70a8cf54ea1ad62edac20cdd09cb8b
ElishaSamPeterPrabhu <ElishaSamPeterPrabhu@users.noreply.github.com>
2025-01-22 06:14:01 +0000

    Deploy preview for PR 129 🛫

---
+++ pr-preview/pr-129/assets/modus-wc-icon.stories-F5HbwuNs.js
@@ -0,0 +1,13 @@
+import{k as s}from"./lit-element-DVRzCIa_.js";import{t as e}from"./if-defined-1ipA9LQg.js";const i={title:"Components/Icon",component:"modus-wc-icon",args:{"aria-label":"Alert icon","custom-class":"",decorative:!1,name:"alert",size:"md",color:""},argTypes:{size:{control:{type:"inline-radio"},options:["xs","sm","md","lg"]},color:{control:{type:"color"}}}},l={render:o=>s`
+      <modus-wc-icon
+        aria-label="${e(o["aria-label"])}"
+        custom-class="${e(o["custom-class"])}"
+        ?decorative="${e(o.decorative)}"
+        name="${o.name}"
+        size="${o.size}"
+        color="${e(o.color)}"
+      >
+      </modus-wc-icon>
+    `},t={...l};var a,r,c;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  ...Template
+}`,...(c=(r=t.parameters)==null?void 0:r.docs)==null?void 0:c.source}}};const d=["Default"];export{t as Default,d as __namedExportsOrder,i as default};
