8f7cf451d9fa87c1788f12eb964bddf3c5167b32
Stuart Rimel <stuart_rimel@trimble.com>
2025-01-27 15:36:14 -0800

    add DaisyColor type

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,4 +1,14 @@
 // These are the supported sizes in DaisyUI
+export type DaisyColor =
+  | 'primary'
+  | 'secondary'
+  | 'accent'
+  | 'neutral'
+  | 'info'
+  | 'success'
+  | 'warning'
+  | 'error';
+
 export type DaisySize = 'xs' | 'sm' | 'md' | 'lg';
 
 export type Density = 'comfortable' | 'compact';
