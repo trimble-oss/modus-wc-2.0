dbbee84373562c48aa13d6373f564058cddce7f8
Stuart Rimel <stuart_rimel@trimble.com>
2025-02-13 15:51:39 -0800

    remove unused color types from types.ts for cleaner code

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,26 +1,7 @@
 // These are the supported sizes in DaisyUI
-export type DaisyColor =
-  | 'primary'
-  | 'secondary'
-  | 'accent'
-  | 'neutral'
-  | 'info'
-  | 'success'
-  | 'warning'
-  | 'error';
-
 export type DaisySize = 'xs' | 'sm' | 'md' | 'lg';
 
 export type Density = 'comfortable' | 'compact';
-
-export type ModusColor =
-  | 'primary'
-  | 'secondary'
-  | 'tertiary'
-  | 'high-contrast'
-  | 'success'
-  | 'warning'
-  | 'danger';
 
 export type ModusSize = Extract<DaisySize, 'sm' | 'md' | 'lg'>;
 
