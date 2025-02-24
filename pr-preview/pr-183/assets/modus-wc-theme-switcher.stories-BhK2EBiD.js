f42529d3eb97588dea4f243880b4970e66ff96dd
gabriel-piltzer-trimble <gabriel-piltzer-trimble@users.noreply.github.com>
2025-02-24 18:23:41 +0000

    Deploy preview for PR 183 🛫

---
+++ pr-preview/pr-183/assets/modus-wc-theme-switcher.stories-BhK2EBiD.js
@@ -0,0 +1,10 @@
+import{w as m}from"./decorator-Dt3Huotz.js";import{k as a}from"./lit-element-DVRzCIa_.js";import{t as c}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";const l={title:"Components/ThemeSwitcher",component:"modus-wc-theme-switcher",args:{"custom-class":void 0,"initial-theme":void 0},argTypes:{},decorators:[m],parameters:{actions:{handles:["themeChange"]}}},i={render:t=>a`
+<modus-wc-theme-provider .initialTheme=${t["initial-theme"]}>
+  <modus-wc-theme-switcher
+    aria-label="Theme toggle"
+    custom-class=${c(t["custom-class"])}
+  ></modus-wc-theme-switcher>
+</modus-wc-theme-provider>
+  `},e={...i};var o,s,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...Template
+}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,l as default};
