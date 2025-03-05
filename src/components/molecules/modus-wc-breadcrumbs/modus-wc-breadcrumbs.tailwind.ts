a98627ab148a68260f949765025e31d66715043b
Stuart Rimel <stuart_rimel@trimble.com>
2025-03-04 16:40:53 -0800

    remove unused underlineLinks prop

--- src/components/molecules/modus-wc-breadcrumbs/modus-wc-breadcrumbs.tailwind.ts
+++ src/components/molecules/modus-wc-breadcrumbs/modus-wc-breadcrumbs.tailwind.ts
@@ -1,9 +1,6 @@
 import { DaisySize } from '../../types';
 
-export const convertPropsToClasses = (props: {
-  size?: DaisySize;
-  underlineLinks?: boolean;
-}): string => {
+export const convertPropsToClasses = (props: { size?: DaisySize }): string => {
   let classes = '';
 
   if (Object.prototype.hasOwnProperty.call(props, 'size') && props.size) {
