23346e9e7a45fc5e1c1d1bbb0b4fc029229794d8
gabriel-piltzer-trimble <gabriel-piltzer-trimble@users.noreply.github.com>
2025-03-18 21:10:03 +0000

    Deploy preview for PR 221 🛫

---
+++ pr-preview/pr-221/assets/index-CyBfjOnu.js
@@ -0,0 +1,1 @@
+import{M as a,u as n}from"./index-Ca2yq2Dp.js";import"./index-BDCjUqtM.js";import"./iframe-DKueu7x3.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
---
+++ pr-preview/pr-221/assets/modus-wc-autocomplete.stories-CTOSO692.js
@@ -0,0 +1,314 @@
+import{w as E}from"./decorator-Dt3Huotz.js";import{k as T}from"./lit-element-DVRzCIa_.js";import{t as a}from"./if-defined-1ipA9LQg.js";import"./v4-CQkTLCs1.js";var v=Object.freeze,x=Object.defineProperty,L=(e,c)=>v(x(e,"raw",{value:v(e.slice())})),h,b;const s=[{label:"Apple",value:"apple",visibleInMenu:!0},{label:"Banana",value:"banana",visibleInMenu:!0},{label:"Blueberry",value:"blueberry",visibleInMenu:!0},{label:"Cherry",value:"cherry",visibleInMenu:!0},{label:"Grape",value:"grape",visibleInMenu:!0},{label:"Lemon",value:"lemon",visibleInMenu:!0},{label:"Orange",value:"orange",visibleInMenu:!0},{label:"Peach",value:"peach",visibleInMenu:!0},{label:"Pear",value:"pear",visibleInMenu:!0},{label:"Strawberry",value:"strawberry",visibleInMenu:!0}],D={title:"Components/Forms/Autocomplete",component:"modus-wc-autocomplete",args:{bordered:!0,"debounce-ms":300,disabled:!1,items:s,label:"Label","min-chars":0,"multi-select":!1,size:"md",value:""},argTypes:{size:{control:{type:"select"},options:["sm","md","lg"]}},decorators:[E],parameters:{actions:{handles:["chipRemove","inputBlur","inputChange","inputFocus","itemSelect"]}}},S={render:e=>{const c=u=>{var n;if(!((n=u.detail)!=null&&n.target))return;const t=u.target.closest("modus-wc-autocomplete");if(t){const o=u.detail.target,l=o.value.toLowerCase(),p=s.map(i=>({...i,selected:l?i.selected:!1,visibleInMenu:i.label.toLowerCase().includes(l)}));t.items=[...p],t.value=o.value}},d=u=>{const t=u.target.closest("modus-wc-autocomplete");if(t){const n=u.detail.label;n&&(t.value=n),s.forEach(l=>l.selected=!1);const o=s.find(l=>l.value===u.detail.value);o&&(o.selected=!0,t.items=[...s])}};return T(h||(h=L([`
+<style>
+  /* Only for Storybook */
+  div[id^="story--components-forms-autocomplete--default"] {
+    height: 400px;
+  }
+</style>
+<script>
+  const handleInputChange = (e) => {
+    if (!e.detail?.target) return;
+
+    const autocomplete = (e.target as HTMLInputElement).closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      const input = e.detail.target as HTMLInputElement;
+      const searchText = input.value.toLowerCase();
+
+      // Create a new array, updating the values of 'selected' and 'visibleInMenu'.
+      const updatedItems = items.map((item) => ({
+        ...item,
+        selected: searchText ? item.selected : false,
+        visibleInMenu: item.label.toLowerCase().includes(searchText),
+      }));
+
+      // Ensuring that a new array is created when updating items is critical to component re-render.
+      autocomplete.items = [...updatedItems];
+      autocomplete.value = input.value;
+    }
+  };
+
+  const handleItemSelect = (e) => {
+    const autocomplete = (e.target as HTMLInputElement).closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      // Clear the previous selection.
+      items.forEach((item) => (item.selected = false));
+
+      // Mark the user selected menu item as selected and create a new array to update items.
+      const foundItem = items.find((item) => item.value === e.detail.value);
+      if (foundItem) {
+        foundItem.selected = true;
+        autocomplete.items = [...items];
+      }
+    }
+  };
+<\/script>
+<modus-wc-autocomplete
+  aria-label="Fruit autocomplete"
+  ?bordered=`,`
+  custom-class=`,`
+  debounce-ms=`,`
+  ?disabled=`,`
+  input-id=`,`
+  input-tab-index=`,`
+  .items=`,`
+  label=`,`
+  min-chars=`,`
+  ?multi-select=`,`
+  name=`,`
+  placeholder=`,`
+  ?read-only=`,`
+  ?required=`,`
+  size=`,`
+  value=`,`
+  @inputChange=`,`
+  @itemSelect=`,`
+></modus-wc-autocomplete>
+    `])),e.bordered,a(e["custom-class"]),a(e["debounce-ms"]),e.disabled,a(e["input-id"]),a(e["input-tab-index"]),e.items,a(e.label),e["min-chars"],!1,a(e.name),a(e.placeholder),e["read-only"],e.required,a(e.size),e.value,c,d)}},m={...S},r={render:e=>{const c=t=>{const n=t.target.closest("modus-wc-autocomplete");if(n){const o=s.find(l=>l.value===t.detail.value);o&&(o.selected=!1,n.items=[...s])}},d=t=>{var o;if(!((o=t.detail)!=null&&o.target))return;const n=t.target.closest("modus-wc-autocomplete");if(n){const l=t.detail.target,p=l.value.toLowerCase(),i=s.map(f=>({...f,visibleInMenu:f.label.toLowerCase().includes(p)}));n.items=[...i],n.value=l.value}},u=t=>{const n=t.target.closest("modus-wc-autocomplete");if(n){n.value="",n.items=s.map(l=>({...l,visibleInMenu:!0}));const o=s.find(l=>l.value===t.detail.value);o&&(o.selected=!0)}};return T(b||(b=L([`
+<style>
+  /* Only for Storybook */
+  div#story--components-forms-autocomplete--multi-select-inner {
+    height: 400px;
+  }
+</style>
+<script>
+  const handleChipRemove = (e) => {
+    const autocomplete = (e.target as HTMLInputElement).closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      // Update the 'selected' value of the removed item.
+      const foundItem = items.find((item) => item.value === e.detail.value);
+      if (foundItem) {
+        foundItem.selected = false;
+        autocomplete.items = [...items];
+      }
+    }
+  };
+
+  const handleInputChange = (e) => {
+    if (!e.detail?.target) return;
+
+    const autocomplete = (e.target as HTMLInputElement).closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      const input = e.detail.target as HTMLInputElement;
+      const searchText = input.value.toLowerCase();
+
+      // Create a new array, updating the values of 'selected' and 'visibleInMenu'.
+      const updatedItems = items.map((item) => ({
+        ...item,
+        selected: searchText ? item.selected : false,
+        visibleInMenu: item.label.toLowerCase().includes(searchText),
+      }));
+
+      // Ensuring that a new array is created when updating items is critical to component re-render.
+      autocomplete.items = [...updatedItems];
+      autocomplete.value = input.value;
+    }
+  };
+
+  const handleItemSelect = (e) => {
+    const autocomplete = (e.target
+    as
+    HTMLInputElement
+  ).
+    closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      // Reset autocomplete 'value' and update the value of 'visibleInMenu' for all items.
+      autocomplete.value = '';
+      autocomplete.items = items.map((item) => ({
+        ...item,
+        visibleInMenu: true,
+      }));
+
+      // Mark the user selected item as selected.
+      const fruit = items.find((fruit) => fruit.value === e.detail.value);
+      if (fruit) {
+        fruit.selected = true;
+      }
+    }
+  };
+<\/script>
+<modus-wc-autocomplete
+  aria-label="Fruit autocomplete"
+  ?bordered=`,`
+  custom-class=`,`
+  debounce-ms=`,`
+  ?disabled=`,`
+  input-id=`,`
+  input-tab-index=`,`
+  .items=`,`
+  label=`,`
+  min-chars=`,`
+  ?multi-select=`,`
+  name=`,`
+  placeholder=`,`
+  ?read-only=`,`
+  ?required=`,`
+  size=`,`
+  value=`,`
+  @chipRemove=`,`
+  @inputChange=`,`
+  @itemSelect=`,`
+></modus-wc-autocomplete>
+    `])),e.bordered,a(e["custom-class"]),a(e["debounce-ms"]),e.disabled,a(e["input-id"]),a(e["input-tab-index"]),e.items,a(e.label),e["min-chars"],!0,a(e.name),a(e.placeholder),e["read-only"],e.required,a(e.size),e.value,c,d,u)}};var I,w,g;m.parameters={...m.parameters,docs:{...(I=m.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  ...Template
+}`,...(g=(w=m.parameters)==null?void 0:w.docs)==null?void 0:g.source}}};var M,y,C;r.parameters={...r.parameters,docs:{...(M=r.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: args => {
+    const handleChipRemove = (e: CustomEvent<IModusWcAutocompleteItem>) => {
+      const autocomplete = (e.target as HTMLInputElement).closest('modus-wc-autocomplete');
+      if (autocomplete) {
+        // Update the 'selected' value of the removed item.
+        const foundItem = items.find(item => item.value === e.detail.value);
+        if (foundItem) {
+          foundItem.selected = false;
+          autocomplete.items = [...items];
+        }
+      }
+    };
+    const handleInputChange = (e: CustomEvent<Event>) => {
+      if (!e.detail?.target) return;
+      const autocomplete = (e.target as HTMLInputElement).closest('modus-wc-autocomplete');
+      if (autocomplete) {
+        const input = e.detail.target as HTMLInputElement;
+        const searchText = input.value.toLowerCase();
+
+        // Create a new array, updating the values of 'selected' and 'visibleInMenu'.
+        const updatedItems = items.map(item => ({
+          ...item,
+          visibleInMenu: item.label.toLowerCase().includes(searchText)
+        }));
+
+        // Ensuring that a new array is created when updating items is critical to component re-render.
+        autocomplete.items = [...updatedItems];
+        autocomplete.value = input.value;
+      }
+    };
+    const handleItemSelect = (e: CustomEvent<IModusWcAutocompleteItem>) => {
+      const autocomplete = (e.target as HTMLInputElement).closest('modus-wc-autocomplete');
+      if (autocomplete) {
+        // Reset autocomplete 'value' and update the value of 'visibleInMenu' for all items.
+        autocomplete.value = '';
+        autocomplete.items = items.map(item => ({
+          ...item,
+          visibleInMenu: true
+        }));
+
+        // Mark the user selected item as selected.
+        const fruit = items.find(fruit => fruit.value === e.detail.value);
+        if (fruit) {
+          fruit.selected = true;
+        }
+      }
+    };
+
+    // prettier-ignore
+    return html\`
+<style>
+  /* Only for Storybook */
+  div#story--components-forms-autocomplete--multi-select-inner {
+    height: 400px;
+  }
+</style>
+<script>
+  const handleChipRemove = (e) => {
+    const autocomplete = (e.target as HTMLInputElement).closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      // Update the 'selected' value of the removed item.
+      const foundItem = items.find((item) => item.value === e.detail.value);
+      if (foundItem) {
+        foundItem.selected = false;
+        autocomplete.items = [...items];
+      }
+    }
+  };
+
+  const handleInputChange = (e) => {
+    if (!e.detail?.target) return;
+
+    const autocomplete = (e.target as HTMLInputElement).closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      const input = e.detail.target as HTMLInputElement;
+      const searchText = input.value.toLowerCase();
+
+      // Create a new array, updating the values of 'selected' and 'visibleInMenu'.
+      const updatedItems = items.map((item) => ({
+        ...item,
+        selected: searchText ? item.selected : false,
+        visibleInMenu: item.label.toLowerCase().includes(searchText),
+      }));
+
+      // Ensuring that a new array is created when updating items is critical to component re-render.
+      autocomplete.items = [...updatedItems];
+      autocomplete.value = input.value;
+    }
+  };
+
+  const handleItemSelect = (e) => {
+    const autocomplete = (e.target
+    as
+    HTMLInputElement
+  ).
+    closest(
+      'modus-wc-autocomplete'
+    );
+
+    if (autocomplete) {
+      // Reset autocomplete 'value' and update the value of 'visibleInMenu' for all items.
+      autocomplete.value = '';
+      autocomplete.items = items.map((item) => ({
+        ...item,
+        visibleInMenu: true,
+      }));
+
+      // Mark the user selected item as selected.
+      const fruit = items.find((fruit) => fruit.value === e.detail.value);
+      if (fruit) {
+        fruit.selected = true;
+      }
+    }
+  };
+<\/script>
+<modus-wc-autocomplete
+  aria-label="Fruit autocomplete"
+  ?bordered=\${args.bordered}
+  custom-class=\${ifDefined(args['custom-class'])}
+  debounce-ms=\${ifDefined(args['debounce-ms'])}
+  ?disabled=\${args.disabled}
+  input-id=\${ifDefined(args['input-id'])}
+  input-tab-index=\${ifDefined(args['input-tab-index'])}
+  .items=\${args.items}
+  label=\${ifDefined(args.label)}
+  min-chars=\${args['min-chars']}
+  ?multi-select=\${true}
+  name=\${ifDefined(args.name)}
+  placeholder=\${ifDefined(args.placeholder)}
+  ?read-only=\${args['read-only']}
+  ?required=\${args.required}
+  size=\${ifDefined(args.size)}
+  value=\${args.value}
+  @chipRemove=\${handleChipRemove}
+  @inputChange=\${handleInputChange}
+  @itemSelect=\${handleItemSelect}
+></modus-wc-autocomplete>
+    \`;
+  }
+}`,...(C=(y=r.parameters)==null?void 0:y.docs)==null?void 0:C.source}}};const _=["Default","MultiSelect"];export{m as Default,r as MultiSelect,_ as __namedExportsOrder,D as default};
