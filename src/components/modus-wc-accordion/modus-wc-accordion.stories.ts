ffc9837cb239b1b3f0dbb599fd8a8f26648e2427
Gabe Piltzer <gabriel_piltzer@trimble.com>
2025-03-19 12:18:40 -0700

    docs: add example migration doc to accordion

--- src/components/modus-wc-accordion/modus-wc-accordion.stories.ts
+++ src/components/modus-wc-accordion/modus-wc-accordion.stories.ts
@@ -69,3 +69,58 @@ const Template: Story = {
 };
 
 export const Default: Story = { ...Template };
+
+export const Migration: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `
+#### Breaking Changes
+
+- In 1.0 the accordion was composed of child accordion-item components. In 2.0
+accordion children are collapse components.
+- The new accordion supports "header" and "content" slots to provide maximum
+flexibility.
+
+#### Prop Mapping
+
+##### accordion
+
+| 1.0 Prop           | 2.0 Prop           | Notes            |
+|--------------------|--------------------|------------------|
+| aria-label         | aria-label         |                  |
+
+##### accordion-item → collapse
+
+| 1.0 Prop           | 2.0 Prop            | Notes            |
+|--------------------|---------------------|------------------|
+| aria-label         | aria-label          |                  |
+| disabled           |                     | Not carried over |
+| expand-button-type |                     | Not carried over |
+| expanded           | expanded            |                  |
+| header-text        | options.title       |                  |
+| icon               | options.icon        |                  |
+| supporting-label   | options.description |                  |
+| size               | options.size        |                  |
+
+#### Event Mapping
+
+##### accordion-item → accordion
+
+The new accordion and collapse have their own events. We recommend using the
+accordion events to migrate.
+
+| 1.0 Event | 2.0 Event      | Notes            |
+|-----------|----------------|------------------|
+| closed    | expandedChange |                  |
+| opened    | expandedChange |                  |
+        `,
+      },
+    },
+    // To hide the actual story rendering and only show docs:
+    controls: { disable: true },
+    canvas: { disable: true },
+  },
+  // Simple render function or leave it empty
+  render: () => html`<div></div>`,
+};
