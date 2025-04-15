bf06d0760a3ca6b0e2dbf397c3a62cd3447ca0c5
gabriel-piltzer-trimble <gabriel-piltzer-trimble@users.noreply.github.com>
2025-04-15 19:11:17 +0000

    Deploy preview for PR 327 🛫

---
+++ pr-preview/pr-327/assets/modus-wc-navbar.stories-DS8rOOjm.js
@@ -0,0 +1,5 @@
+import{x as s}from"./lit-element-C8zulti1.js";import{o as m}from"./if-defined-yv6owfG8.js";const d={title:"Components/Navbar",component:"modus-wc-navbar",parameters:{layout:"padded"}},c={render:t=>s`
+<modus-wc-navbar custom-class="${m(t["custom-class"])}"></modus-wc-navbar>
+    `},a={...c};var e,r,o;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  ...Template
+}`,...(o=(r=a.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const u=["Default"];export{a as Default,u as __namedExportsOrder,d as default};
