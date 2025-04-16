d29fc61d62c805b9003726437ed43a6212893f39
Gabe Piltzer <gabriel_piltzer@trimble.com>
2025-04-16 16:47:40 -0700

    feat: navbar button visibility

--- src/components/modus-wc-navbar/modus-wc-navbar.stories.ts
+++ src/components/modus-wc-navbar/modus-wc-navbar.stories.ts
@@ -2,7 +2,7 @@ import { withActions } from '@storybook/addon-actions/decorator';
 import { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined.js';
-import { IUserCard } from './modus-wc-navbar';
+import { INavbarVisibility, IUserCard } from './modus-wc-navbar';
 
 const user: IUserCard = {
   avatarAlt: 'Sonic',
@@ -11,9 +11,18 @@ const user: IUserCard = {
   name: 'Sonic the Hedgehog',
 };
 
+const visibility: INavbarVisibility = {
+  apps: true,
+  help: true,
+  mainMenu: true,
+  notifications: true,
+  user: true,
+};
+
 interface NavbarArgs {
   'custom-class'?: string;
   user: IUserCard;
+  visibility?: INavbarVisibility;
 }
 
 const meta: Meta<NavbarArgs> = {
@@ -21,6 +30,7 @@ const meta: Meta<NavbarArgs> = {
   component: 'modus-wc-navbar',
   args: {
     user,
+    visibility,
   },
   argTypes: {
     user: {
@@ -36,6 +46,22 @@ const meta: Meta<NavbarArgs> = {
             - myTrimbleButton (string, optional): Text override for the Access MyTrimble button, allows for translation
             - name (string): The name of the user
             - signOutButton (string, optional): Text override for the Sign out button, allows for translation
+          `,
+        },
+      },
+    },
+    visibility: {
+      description: 'Controls visibility of individual navbar buttons',
+      table: {
+        type: {
+          detail: `
+            Interface: INavbarVisibility
+            Properties:
+            - apps (boolean, optional): Controls visibility of the apps button
+            - help (boolean, optional): Controls visibility of the help button
+            - mainMenu (boolean, optional): Controls visibility of the main menu button
+            - notifications (boolean, optional): Controls visibility of the notifications button
+            - user (boolean, optional): Controls visibility of the user button
           `,
         },
       },
@@ -71,8 +97,9 @@ const Template: Story = {
 <modus-wc-navbar
   custom-class=${ifDefined(args['custom-class'])}
   .user=${args.user}
+  .visibility=${args.visibility}
 >
-  <div slot="menu">Menu contents</div>
+  <div slot="main-menu">Main menu contents</div>
   <div slot="notifications">Notification contents</div>
   <div slot="apps">App drawer contents</div>
 </modus-wc-navbar>
