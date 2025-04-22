19cd8fcd1d843e02e14f1533d153cab22cbb79e7
prashanthr6383 <168108000+prashanthr6383@users.noreply.github.com>
2025-04-22 15:56:32 +0530

    278 - add DIRECTIVES to ng19

--- integrations/angular/ng19/projects/trimble-oss/moduswebcomponents-angular/src/lib/modus-wc-angular.module.ts
+++ integrations/angular/ng19/projects/trimble-oss/moduswebcomponents-angular/src/lib/modus-wc-angular.module.ts
@@ -1,10 +1,10 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { defineCustomElements } from '@trimble-oss/moduswebcomponents/loader';
+import { DIRECTIVES } from './stencil-generated';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: [],
+  imports: [...DIRECTIVES],
+  exports: [...DIRECTIVES],
   providers: [
     {
         provide: APP_INITIALIZER,
