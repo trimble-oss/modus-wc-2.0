5bf95208e24936927369b089bc6609c3a9aa1880
gabriel-piltzer-trimble <gabriel-piltzer-trimble@users.noreply.github.com>
2025-04-24 18:51:13 +0000

    Deploy preview for PR 361 🛫

---
+++ pr-preview/pr-361/assets/modus-wc-modal.stories-Da5iruhB.js
@@ -0,0 +1,176 @@
+import{x as i}from"./lit-element-C8zulti1.js";import{o as l}from"./if-defined-yv6owfG8.js";function v(o=8){return Math.random().toString(36).substring(2,2+o)}const M={title:"Components/Modal",component:"modus-wc-modal",args:{backdrop:"default","custom-class":"",fullscreen:!1,"modal-id":"my_modal_1",position:"center","show-close":!0,"show-fullscreen-toggle":!1},argTypes:{backdrop:{control:{type:"select"},options:["default","static"]},position:{control:{type:"select"},options:["bottom","center","top"]}},parameters:{layout:"centered"}},a={render:o=>{const t=`${o["modal-id"]}${v(4)}}`,n=r=>{const e=document.getElementById(t);e&&(r==="show"?e.showModal():e.close())};return i`
+<modus-wc-button @buttonClick=${()=>n("show")}>
+  Open modal
+</modus-wc-button>
+<modus-wc-modal
+  aria-label="Example modal"
+  custom-class=${l(o["custom-class"])}
+  fullscreen=${o.fullscreen}
+  modal-id=${t}
+  backdrop=${o.backdrop}
+  position=${o.position}
+  show-close=${o["show-close"]}
+  show-fullscreen-toggle=${o["show-fullscreen-toggle"]}
+>
+  <span slot="header">Modal Title</span>
+  <span slot="content"> This is sample modal content. </span>
+  <modus-wc-button slot="footer" @buttonClick=${()=>n("hide")}>
+    Close
+  </modus-wc-button>
+</modus-wc-modal>
+    `}},s={render:o=>{const t=`${o["modal-id"]}${v(4)}}`,n=r=>{const e=document.getElementById(t);e&&(r==="show"?e.showModal():e.close())};return i`
+<style>
+  #modal2 .modus-wc-modal-box {
+    width: 80%;
+    max-width: none;
+    height: 60%;
+    max-height: none;
+  }
+</style>
+<modus-wc-button @buttonClick=${()=>n("show")}>
+  Open modal
+</modus-wc-button>
+<modus-wc-modal
+  aria-label="Example modal"
+  custom-class="expanded-modal"
+  modal-id=${t}
+  backdrop=${l(o.backdrop)}
+  position=${l(o.position)}
+  show-close=${l(o["show-close"])}
+>
+  <span slot="header">Modal Title</span>
+  <p slot="content">Sample modal content.</p>
+  <modus-wc-button slot="footer" @buttonClick=${()=>n("hide")}>
+    Close
+  </modus-wc-button>
+</modus-wc-modal>
+    `}},d={parameters:{docs:{description:{story:"\n#### Breaking Changes\n\n  - Modal identification is now required via the `modal-id` prop.\n  - 2.0 requires the use of slots for a fully customizable `header`, `content`, and `footer`.\n  Primary and secondary buttons as well as `header-text` are no longer built-in.\n  - In 1.0, modals had built-in open/close state management with methods. 2.0 uses the native HTML dialog\n  element with `modal-id` to target the dialog with native `showModal()` and `close()` methods.\n\n#### Prop Mapping\n\n| 1.0 Prop                     | 2.0 Prop                | Notes                                         |\n|------------------------------|-------------------------|-----------------------------------------------|\n| aria-label                   | aria-label              |                                               |\n| backdrop                     | backdrop                |                                               |\n| fullscreen                   | fullscreen              |                                               |\n| header-text                  |                         | Not carried over, use `header` slot instead |\n| primary-button-aria-label    |                         | Not carried over, use `footer` slot instead |\n| primary-button-disabled      |                         | Not carried over, use `footer` slot instead |\n| primary-button-text          |                         | Not carried over, use `footer` slot instead |\n| secondary-button-aria-label  |                         | Not carried over, use `footer` slot instead |\n| secondary-button-disabled    |                         | Not carried over, use `footer` slot instead |\n| secondary-button-text        |                         | Not carried over, use `footer` slot instead |\n| show-fullscreen-toggle       | show-fullscreen-toggle  |                                               |\n| z-index                      |                         | Not carried over, use CSS instead             |\n\n#### Event Mapping\n\n| 1.0 Event            | 2.0 Event | Notes                                                                             |\n|----------------------|-----------|-----------------------------------------------------------------------------------|\n| closed               |           | Not carried over, use dialog `close()` event instead                            |\n| opened               |           | Not carried over, use dialog `showModal()` event instead                        |\n| primaryButtonClick   |           | Not carried over, handle with events on custom buttons in `footer` slot instead |\n| secondaryButtonClick |           | Not carried over, handle with events on custom buttons in `footer` slot instead |\n        "}},controls:{disable:!0},canvas:{disable:!0}},render:()=>i`<div></div>`};var c,m,u;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: args => {
+    const modalId = \`\${args['modal-id']}\${generateRandomId(4)}}\`;
+    const handleModalVisibility = (action: 'show' | 'hide') => {
+      const modal = document.getElementById(modalId) as HTMLDialogElement;
+      if (modal) {
+        if (action === 'show') {
+          modal.showModal();
+        } else {
+          modal.close();
+        }
+      }
+    };
+
+    // prettier-ignore
+    return html\`
+<modus-wc-button @buttonClick=\${() => handleModalVisibility('show')}>
+  Open modal
+</modus-wc-button>
+<modus-wc-modal
+  aria-label="Example modal"
+  custom-class=\${ifDefined(args['custom-class'])}
+  fullscreen=\${args.fullscreen}
+  modal-id=\${modalId}
+  backdrop=\${args.backdrop}
+  position=\${args.position}
+  show-close=\${args['show-close']}
+  show-fullscreen-toggle=\${args['show-fullscreen-toggle']}
+>
+  <span slot="header">Modal Title</span>
+  <span slot="content"> This is sample modal content. </span>
+  <modus-wc-button slot="footer" @buttonClick=\${() => handleModalVisibility('hide')}>
+    Close
+  </modus-wc-button>
+</modus-wc-modal>
+    \`;
+  }
+}`,...(u=(m=a.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var p,h,b;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => {
+    const modalId = \`\${args['modal-id']}\${generateRandomId(4)}}\`;
+    const handleModalVisibility = (action: 'show' | 'hide') => {
+      const modal = document.getElementById(modalId) as HTMLDialogElement;
+      if (modal) {
+        if (action === 'show') {
+          modal.showModal();
+        } else {
+          modal.close();
+        }
+      }
+    };
+
+    // prettier-ignore
+    return html\`
+<style>
+  #modal2 .modus-wc-modal-box {
+    width: 80%;
+    max-width: none;
+    height: 60%;
+    max-height: none;
+  }
+</style>
+<modus-wc-button @buttonClick=\${() => handleModalVisibility('show')}>
+  Open modal
+</modus-wc-button>
+<modus-wc-modal
+  aria-label="Example modal"
+  custom-class="expanded-modal"
+  modal-id=\${modalId}
+  backdrop=\${ifDefined(args.backdrop)}
+  position=\${ifDefined(args.position)}
+  show-close=\${ifDefined(args['show-close'])}
+>
+  <span slot="header">Modal Title</span>
+  <p slot="content">Sample modal content.</p>
+  <modus-wc-button slot="footer" @buttonClick=\${() => handleModalVisibility('hide')}>
+    Close
+  </modus-wc-button>
+</modus-wc-modal>
+    \`;
+  }
+}`,...(b=(h=s.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};var w,f,g;d.parameters={...d.parameters,docs:{...(w=d.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      description: {
+        story: \`
+#### Breaking Changes
+
+  - Modal identification is now required via the \\\`modal-id\\\` prop.
+  - 2.0 requires the use of slots for a fully customizable \\\`header\\\`, \\\`content\\\`, and \\\`footer\\\`.
+  Primary and secondary buttons as well as \\\`header-text\\\` are no longer built-in.
+  - In 1.0, modals had built-in open/close state management with methods. 2.0 uses the native HTML dialog
+  element with \\\`modal-id\\\` to target the dialog with native \\\`showModal()\\\` and \\\`close()\\\` methods.
+
+#### Prop Mapping
+
+| 1.0 Prop                     | 2.0 Prop                | Notes                                         |
+|------------------------------|-------------------------|-----------------------------------------------|
+| aria-label                   | aria-label              |                                               |
+| backdrop                     | backdrop                |                                               |
+| fullscreen                   | fullscreen              |                                               |
+| header-text                  |                         | Not carried over, use \\\`header\\\` slot instead |
+| primary-button-aria-label    |                         | Not carried over, use \\\`footer\\\` slot instead |
+| primary-button-disabled      |                         | Not carried over, use \\\`footer\\\` slot instead |
+| primary-button-text          |                         | Not carried over, use \\\`footer\\\` slot instead |
+| secondary-button-aria-label  |                         | Not carried over, use \\\`footer\\\` slot instead |
+| secondary-button-disabled    |                         | Not carried over, use \\\`footer\\\` slot instead |
+| secondary-button-text        |                         | Not carried over, use \\\`footer\\\` slot instead |
+| show-fullscreen-toggle       | show-fullscreen-toggle  |                                               |
+| z-index                      |                         | Not carried over, use CSS instead             |
+
+#### Event Mapping
+
+| 1.0 Event            | 2.0 Event | Notes                                                                             |
+|----------------------|-----------|-----------------------------------------------------------------------------------|
+| closed               |           | Not carried over, use dialog \\\`close()\\\` event instead                            |
+| opened               |           | Not carried over, use dialog \\\`showModal()\\\` event instead                        |
+| primaryButtonClick   |           | Not carried over, handle with events on custom buttons in \\\`footer\\\` slot instead |
+| secondaryButtonClick |           | Not carried over, handle with events on custom buttons in \\\`footer\\\` slot instead |
+        \`
+      }
+    },
+    controls: {
+      disable: true
+    },
+    canvas: {
+      disable: true
+    }
+  },
+  render: () => html\`<div></div>\`
+}`,...(g=(f=d.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};const k=["Default","CustomWidthAndHeight","Migration"];export{s as CustomWidthAndHeight,a as Default,d as Migration,k as __namedExportsOrder,M as default};
