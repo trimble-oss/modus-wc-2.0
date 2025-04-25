89bebd4e9a278b01de2c239e32f6b1c04ed90f4f
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2025-04-25 08:40:52 +0530

    Upgrade: React version 19 (#341)

---
+++ integrations/react/test-react-v19/src/examples/ModusIconExamples.tsx
@@ -0,0 +1,36 @@
+import { ModusWcIcon as ModusIcon } from "@trimble-oss/moduswebcomponents-react";
+
+export default function ModusBadgeExamples() {
+	return (
+		<>
+			<h3>Icon</h3>
+			<div className="grid">
+				<div className="grid-row">
+					<ModusIcon aria-label="Alert icon" name="alert" />
+					<ModusIcon aria-label="Accessible" name="accessibility_circle" />
+				</div>
+				<div className="grid-row">
+					Small <ModusIcon aria-label="Alert icon" name="alert" size="sm" />
+					Medium <ModusIcon aria-label="Alert icon" name="alert" size="md" />
+					Large <ModusIcon aria-label="Alert icon" name="alert" size="lg" />
+				</div>
+				<div className="grid-row">
+					<ModusIcon name="file_new" decorative />
+					<ModusIcon name="file_type_pdf" decorative />
+				</div>
+				<div className="grid-row">
+					<ModusIcon
+						name="add_bold"
+						decorative
+						style={{ color: "var(--modus-wc-color-trimble-blue)" }}
+					/>
+					<ModusIcon
+						name="add_bold"
+						decorative
+						style={{ color: "var(--modus-wc-color-red)" }}
+					/>
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ integrations/react/test-react-v19/src/examples/index.tsx
@@ -0,0 +1,3 @@
+export { default as Badge } from "./ModusBadgeExamples";
+export { default as Icon } from "./ModusIconExamples";
+export { default as Tabs } from "./ModusTabsExamples";
