68d6ea8dd72c34c2fc848581cd86472da82717f9
Gabe Piltzer <gabriel_piltzer@trimble.com>
2025-06-23 14:12:49 -0700

    feat: add dropdown menu component

---
+++ src/components/modus-wc-dropdown-menu/modus-wc-dropdown-menu.stories.ts
@@ -0,0 +1,152 @@
+import { withActions } from '@storybook/addon-actions/decorator';
+import { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { DaisySize, ModusSize, PopoverPlacement } from '../types';
+
+interface DropdownMenuArgs {
+  'button-color'?: 'primary' | 'secondary' | 'tertiary' | 'warning' | 'danger';
+  'button-size'?: DaisySize;
+  'button-variant'?: 'borderless' | 'filled' | 'outlined';
+  'custom-class'?: string;
+  disabled?: boolean;
+  'menu-bordered'?: boolean;
+  'menu-offset'?: number;
+  'menu-placement'?: PopoverPlacement;
+  'menu-size'?: ModusSize;
+  'menu-visible': boolean;
+}
+
+const meta: Meta<DropdownMenuArgs> = {
+  title: 'Components/Dropdown Menu',
+  component: 'modus-wc-dropdown-menu',
+  args: {
+    'button-color': 'primary',
+    'button-size': 'md',
+    'button-variant': 'filled',
+    disabled: false,
+    'menu-bordered': true,
+    'menu-offset': 14,
+    'menu-placement': 'bottom-start',
+    'menu-size': 'md',
+    'menu-visible': false,
+  },
+  argTypes: {
+    'button-color': {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'tertiary', 'warning', 'danger'],
+    },
+    'button-size': {
+      control: { type: 'select' },
+      options: ['xs', 'sm', 'md', 'lg'],
+    },
+    'button-variant': {
+      control: { type: 'select' },
+      options: ['borderless', 'filled', 'outlined'],
+    },
+    'menu-placement': {
+      control: { type: 'select' },
+      options: [
+        'top',
+        'top-start',
+        'top-end',
+        'bottom',
+        'bottom-start',
+        'bottom-end',
+        'left',
+        'left-start',
+        'left-end',
+        'right',
+        'right-start',
+        'right-end',
+      ],
+    },
+    'menu-size': {
+      control: { type: 'select' },
+      options: ['sm', 'md', 'lg'],
+    },
+  },
+  decorators: [withActions],
+  parameters: {
+    actions: {
+      handles: ['menuVisibilityChange'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<DropdownMenuArgs>;
+
+const Template: Story = {
+  render: (args) => {
+    let selectedValue = '';
+
+    const handleItemSelect = (event: CustomEvent) => {
+      selectedValue = event.detail.value;
+      const displayElement = document.querySelector('#selected-value');
+      if (displayElement) {
+        displayElement.textContent = selectedValue;
+      }
+
+      // Close the dropdown menu when an item is selected
+      const dropdownMenu = event.target as HTMLElement;
+      const dropdownMenuElement = dropdownMenu.closest(
+        'modus-wc-dropdown-menu'
+      );
+      if (dropdownMenuElement) {
+        dropdownMenuElement.menuVisible = false;
+      }
+    };
+
+    // prettier-ignore
+    return html`
+<style>
+  div#story--components-dropdown-menu--default--primary-inner {
+    height: 120px;
+  }
+
+  [slot="button"] {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+
+  .value {
+    font-size: 14px;
+    padding-top: 12px;
+  }
+</style>
+
+<modus-wc-dropdown-menu
+  button-color=${ifDefined(args['button-color'])}
+  button-size=${ifDefined(args['button-size'])}
+  button-variant=${ifDefined(args['button-variant'])}
+  custom-class=${ifDefined(args['custom-class'])}
+  ?disabled=${args.disabled}
+  ?menu-bordered=${args['menu-bordered']}
+  menu-offset=${ifDefined(args['menu-offset'])}
+  menu-placement=${ifDefined(args['menu-placement'])}
+  menu-size=${ifDefined(args['menu-size'])}
+  ?menu-visible=${args['menu-visible']}
+>
+  <div slot="button">
+    Button
+    <modus-wc-icon name="expand_more" size="sm" />
+  </div>
+  <div slot="menu">
+    <modus-wc-menu-item label="Item One" value="1" @itemSelect=${handleItemSelect}></modus-wc-menu-item>
+    <modus-wc-menu-item label="Item Two" value="2" @itemSelect=${handleItemSelect} /></modus-wc-menu-item>
+    <modus-wc-menu-item label="Item Three" value="3" @itemSelect=${handleItemSelect} /></modus-wc-menu-item>
+  </div>
+</modus-wc-dropdown-menu>
+
+<div class="value">
+  Selected Value:
+  <span id="selected-value"></span>
+</div>
+    `;
+  },
+};
+
+export const Default: Story = { ...Template };
---
+++ src/components/modus-wc-dropdown-menu/modus-wc-dropdown-menu.tsx
@@ -0,0 +1,170 @@
+import { computePosition, flip, offset, shift } from '@floating-ui/dom';
+import {
+  Component,
+  Element,
+  EventEmitter,
+  h,
+  Host,
+  Listen,
+  Prop,
+  State,
+  Event as StencilEvent,
+  Watch,
+} from '@stencil/core';
+import { DaisySize, ModusSize, PopoverPlacement } from '../types';
+import { Attributes, inheritAriaAttributes } from '../utils';
+
+/**
+ * A customizable dropdown menu component used to render a button and toggleable menu.
+ */
+@Component({
+  tag: 'modus-wc-dropdown-menu',
+  styleUrl: 'modus-wc-dropdown-menu.scss',
+  shadow: false,
+})
+export class ModusWcDropdownMenu {
+  private buttonRef?: HTMLElement;
+  private inheritedAttributes: Attributes = {};
+  private menuRef?: HTMLElement;
+
+  /** Reference to the host element */
+  @Element() el!: HTMLElement;
+
+  /** The color variant of the button. */
+  @Prop() buttonColor?:
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'warning'
+    | 'danger' = 'primary';
+
+  /** The size of the button. */
+  @Prop() buttonSize?: DaisySize = 'md';
+
+  /** The variant of the button. */
+  @Prop() buttonVariant?: 'borderless' | 'filled' | 'outlined' = 'filled';
+
+  /** Custom CSS class to apply to the host element. */
+  @Prop() customClass?: string = '';
+
+  /** If true, the button will be disabled. */
+  @Prop() disabled?: boolean = false;
+
+  /** Indicates that the menu should have a border. */
+  @Prop() menuBordered?: boolean = true;
+
+  /** Distance between the button and menu in pixels. */
+  @Prop() menuOffset?: number = 10;
+
+  /** The placement of the menu relative to the button. */
+  @Prop() menuPlacement?: PopoverPlacement = 'bottom-start';
+
+  /** The size of the menu. */
+  @Prop() menuSize?: ModusSize = 'md';
+
+  /** Indicates that the menu is visible. */
+  @Prop({ mutable: true }) menuVisible: boolean = false;
+
+  /** Event emitted when the menuVisible prop changes. */
+  @StencilEvent() menuVisibilityChange!: EventEmitter<{ isVisible: boolean }>;
+
+  @State() private menuPosition = { x: 0, y: 0 };
+
+  @Listen('click', { target: 'document' })
+  handleDocumentClick(event: Event) {
+    // Close the menu when the user clicks outside the component
+    if (!this.el.contains(event.target as Node) && this.menuVisible) {
+      this.menuVisible = false;
+      this.menuVisibilityChange.emit({ isVisible: false });
+    }
+  }
+
+  @Listen('keydown')
+  handleKeyDown(event: KeyboardEvent) {
+    // Close the menu when the user clicks escape
+    if (event.key === 'Escape' && this.menuVisible) {
+      this.menuVisible = false;
+      this.menuVisibilityChange.emit({ isVisible: false });
+    }
+  }
+
+  @Watch('menuVisible')
+  async onMenuVisibilityChange(newValue: boolean) {
+    if (newValue) {
+      await this.updateMenuPosition();
+    }
+  }
+
+  componentDidLoad() {
+    this.buttonRef = this.el.querySelector('modus-wc-button') as HTMLElement;
+  }
+
+  componentWillLoad() {
+    this.inheritedAttributes = inheritAriaAttributes(this.el);
+  }
+
+  private getClasses(): string {
+    const classList: string[] = ['modus-wc-dropdown-menu'];
+
+    // The order CSS classes are added matters to CSS specificity
+    if (this.customClass) classList.push(this.customClass);
+
+    return classList.join(' ');
+  }
+
+  private handleButtonClick = () => {
+    const newVisibility = !this.menuVisible;
+    this.menuVisible = newVisibility;
+    this.menuVisibilityChange.emit({ isVisible: newVisibility });
+  };
+
+  private updateMenuPosition = async () => {
+    if (!this.buttonRef || !this.menuRef) return;
+
+    const { x, y } = await computePosition(this.buttonRef, this.menuRef, {
+      placement: this.menuPlacement,
+      middleware: [offset(this.menuOffset), flip(), shift({ padding: 8 })],
+    });
+
+    this.menuPosition = { x, y };
+  };
+
+  render() {
+    return (
+      <Host class={this.getClasses()} {...this.inheritedAttributes}>
+        <modus-wc-button
+          aria-expanded={this.menuVisible.toString()}
+          aria-haspopup="true"
+          color={this.buttonColor}
+          disabled={this.disabled}
+          onButtonClick={this.handleButtonClick}
+          size={this.buttonSize}
+          variant={this.buttonVariant}
+        >
+          <slot name="button" />
+        </modus-wc-button>
+
+        <div
+          aria-hidden={!this.menuVisible}
+          class="menu-wrapper"
+          ref={(el) => (this.menuRef = el)}
+          style={{
+            // Positioning
+            position: 'absolute',
+            top: `${this.menuPosition.y}px`,
+            left: `${this.menuPosition.x}px`,
+            zIndex: '1000',
+            // Visibility
+            visibility: this.menuVisible ? 'visible' : 'hidden',
+            opacity: this.menuVisible ? '1' : '0',
+            pointerEvents: this.menuVisible ? 'auto' : 'none',
+          }}
+        >
+          <modus-wc-menu bordered={this.menuBordered} size={this.menuSize}>
+            <slot name="menu" />
+          </modus-wc-menu>
+        </div>
+      </Host>
+    );
+  }
+}
