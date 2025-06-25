9b30e8d7335240fb4631f47cf56a5787fbe62bd4
mitch-trimble <mitch-trimble@users.noreply.github.com>
2025-06-25 11:07:01 +0000

    Deploy preview for PR 437 🛫

---
+++ pr-preview/pr-437/assets/modus-wc-menu.stories-BjN6XP7J.js
@@ -0,0 +1,275 @@
+import{w as m}from"./decorator-D4YmxizW.js";import{x as r}from"./lit-element-C8zulti1.js";import{o as s}from"./if-defined-yv6owfG8.js";import"./chunk-4XZ63LWV-CnYBn8W6.js";import"./v4-CtRu48qb.js";const h={title:"Components/Menu",component:"modus-wc-menu",args:{orientation:"vertical",size:"md"},argTypes:{orientation:{control:{type:"select"},options:["horizontal","vertical"]},size:{control:{type:"select"},options:["xs","sm","md","lg"]}},decorators:[m],parameters:{actions:{handles:["menuFocusout"]}}},n={render:e=>r`
+<modus-wc-menu
+  aria-label="Menu"
+  ?bordered=${e.bordered}
+  custom-class=${s(e["custom-class"])}
+  orientation=${s(e.orientation)}
+  size=${s(e.size)}
+>
+  <modus-wc-menu-item
+    label="Small"
+    value="1"
+    size="sm"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item label="Medium" value="2"></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Large"
+    value="3"
+    size="lg"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Bordered"
+    value="3"
+    bordered="true"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="With Sub-label"
+    value="3"
+    sub-label="Sub-label"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Selected"
+    value="3"
+    selected="true"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="With Start Icon"
+    value="3"
+    start-icon="info"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Disabled"
+    value="3"
+    disabled="true"
+  ></modus-wc-menu-item>
+</modus-wc-menu>
+    `},i={render:()=>r`
+<style>
+  .menu-width {
+    width: 400px;
+  }
+  .flex-row {
+    display: flex;
+    align-items: center;
+  }
+  .nested-row {
+    padding-inline-start: 3rem;
+  }
+  .justify-end {
+    margin-left: auto;
+  }
+  .green-square {
+    height: 18px;
+    width: 18px;
+    background-color: green;
+  }
+  .red-square {
+    height: 18px;
+    width: 18px;
+    background-color: red;
+  }
+</style>
+<modus-wc-menu
+  aria-label="Custom menu"
+  custom-class="menu-width"
+>
+  <li>
+    <div class="flex-row">
+      <modus-wc-icon decorative="true" name="expand_more"></modus-wc-icon>
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Visible icon" name="visibility_on"></modus-wc-icon>
+      </modus-wc-button>
+      <div>Parent</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="flex-row nested-row">
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Visible icon" name="visibility_on"></modus-wc-icon>
+      </modus-wc-button>
+      <div class="green-square"></div>
+      <div>Child</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="flex-row nested-row">
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Invisible icon" name="visibility_off"></modus-wc-icon>
+      </modus-wc-button>
+      <div class="red-square"></div>
+      <div>Child</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="flex-row">
+      <modus-wc-icon decorative="true" name="chevron_right"></modus-wc-icon>
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Visible icon" name="visibility_on"></modus-wc-icon>
+      </modus-wc-button>
+      <div>Parent</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+</modus-wc-menu>
+    `};var o,a,t;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    // prettier-ignore
+    return html\`
+<modus-wc-menu
+  aria-label="Menu"
+  ?bordered=\${args.bordered}
+  custom-class=\${ifDefined(args['custom-class'])}
+  orientation=\${ifDefined(args.orientation)}
+  size=\${ifDefined(args.size)}
+>
+  <modus-wc-menu-item
+    label="Small"
+    value="1"
+    size="sm"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item label="Medium" value="2"></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Large"
+    value="3"
+    size="lg"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Bordered"
+    value="3"
+    bordered="true"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="With Sub-label"
+    value="3"
+    sub-label="Sub-label"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Selected"
+    value="3"
+    selected="true"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="With Start Icon"
+    value="3"
+    start-icon="info"
+  ></modus-wc-menu-item>
+  <modus-wc-menu-item
+    label="Disabled"
+    value="3"
+    disabled="true"
+  ></modus-wc-menu-item>
+</modus-wc-menu>
+    \`;
+  }
+}`,...(t=(a=n.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var c,l,u;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    // prettier-ignore
+    return html\`
+<style>
+  .menu-width {
+    width: 400px;
+  }
+  .flex-row {
+    display: flex;
+    align-items: center;
+  }
+  .nested-row {
+    padding-inline-start: 3rem;
+  }
+  .justify-end {
+    margin-left: auto;
+  }
+  .green-square {
+    height: 18px;
+    width: 18px;
+    background-color: green;
+  }
+  .red-square {
+    height: 18px;
+    width: 18px;
+    background-color: red;
+  }
+</style>
+<modus-wc-menu
+  aria-label="Custom menu"
+  custom-class="menu-width"
+>
+  <li>
+    <div class="flex-row">
+      <modus-wc-icon decorative="true" name="expand_more"></modus-wc-icon>
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Visible icon" name="visibility_on"></modus-wc-icon>
+      </modus-wc-button>
+      <div>Parent</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="flex-row nested-row">
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Visible icon" name="visibility_on"></modus-wc-icon>
+      </modus-wc-button>
+      <div class="green-square"></div>
+      <div>Child</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="flex-row nested-row">
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Invisible icon" name="visibility_off"></modus-wc-icon>
+      </modus-wc-button>
+      <div class="red-square"></div>
+      <div>Child</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="flex-row">
+      <modus-wc-icon decorative="true" name="chevron_right"></modus-wc-icon>
+      <modus-wc-button aria-label="Visible button" size="sm" shape="circle" variant="borderless">
+        <modus-wc-icon aria-label="Visible icon" name="visibility_on"></modus-wc-icon>
+      </modus-wc-button>
+      <div>Parent</div>
+      <div class="justify-end">
+        <modus-wc-button aria-label="Actions button" size="sm" shape="circle" variant="borderless">
+          <modus-wc-icon aria-label="Actions icon" name="more_vertical"></modus-wc-icon>
+        </modus-wc-button>
+      </div>
+    </div>
+  </li>
+</modus-wc-menu>
+    \`;
+  }
+}`,...(u=(l=i.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};const f=["Default","CustomMenu"];export{i as CustomMenu,n as Default,f as __namedExportsOrder,h as default};
