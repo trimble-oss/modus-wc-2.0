3e857acc0af1f11e3383adfacd64c67921b6f836
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2025-07-03 12:35:58 +0530

    test vue

---
+++ integrations/vue/v-latest/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ModusWcButton } from './components';
+</script>
+
+<template>
+  <div>
+    <ModusWcButton>Hello Vue!</ModusWcButton>
+  </div>
+</template> 
---
+++ integrations/vue/v-latest/src/components.ts
@@ -0,0 +1,708 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer, type StencilVueComponent } from '@stencil/vue-output-target/runtime';
+
+import type { JSX } from '@trimble-oss/moduswebcomponents';
+
+
+
+
+export const ModusWcAccordion: StencilVueComponent<JSX.ModusWcAccordion> = /*@__PURE__*/ defineContainer<JSX.ModusWcAccordion>('modus-wc-accordion', undefined, [
+  'customClass',
+  'expandedChange'
+], [
+  'expandedChange'
+]);
+
+
+export const ModusWcAlert: StencilVueComponent<JSX.ModusWcAlert> = /*@__PURE__*/ defineContainer<JSX.ModusWcAlert>('modus-wc-alert', undefined, [
+  'alertDescription',
+  'alertTitle',
+  'customClass',
+  'dismissible',
+  'icon',
+  'variant',
+  'role',
+  'dismissClick'
+], [
+  'dismissClick'
+]);
+
+
+export const ModusWcAutocomplete: StencilVueComponent<JSX.ModusWcAutocomplete> = /*@__PURE__*/ defineContainer<JSX.ModusWcAutocomplete>('modus-wc-autocomplete', undefined, [
+  'bordered',
+  'customClass',
+  'debounceMs',
+  'disabled',
+  'includeClear',
+  'includeSearch',
+  'inputId',
+  'inputTabIndex',
+  'items',
+  'label',
+  'leaveMenuOpen',
+  'minChars',
+  'multiSelect',
+  'name',
+  'noResults',
+  'placeholder',
+  'readOnly',
+  'required',
+  'showMenuOnFocus',
+  'size',
+  'showSpinner',
+  'value',
+  'chipRemove',
+  'inputBlur',
+  'inputChange',
+  'inputFocus',
+  'itemSelect'
+], [
+  'chipRemove',
+  'inputBlur',
+  'inputChange',
+  'inputFocus',
+  'itemSelect'
+]);
+
+
+export const ModusWcAvatar: StencilVueComponent<JSX.ModusWcAvatar> = /*@__PURE__*/ defineContainer<JSX.ModusWcAvatar>('modus-wc-avatar', undefined, [
+  'alt',
+  'customClass',
+  'imgSrc',
+  'shape',
+  'size'
+]);
+
+
+export const ModusWcBadge: StencilVueComponent<JSX.ModusWcBadge> = /*@__PURE__*/ defineContainer<JSX.ModusWcBadge>('modus-wc-badge', undefined, [
+  'color',
+  'customClass',
+  'size',
+  'variant'
+]);
+
+
+export const ModusWcBreadcrumbs: StencilVueComponent<JSX.ModusWcBreadcrumbs> = /*@__PURE__*/ defineContainer<JSX.ModusWcBreadcrumbs>('modus-wc-breadcrumbs', undefined, [
+  'items',
+  'customClass',
+  'size',
+  'breadcrumbClick'
+], [
+  'breadcrumbClick'
+]);
+
+
+export const ModusWcButton: StencilVueComponent<JSX.ModusWcButton> = /*@__PURE__*/ defineContainer<JSX.ModusWcButton>('modus-wc-button', undefined, [
+  'color',
+  'customClass',
+  'disabled',
+  'fullWidth',
+  'pressed',
+  'shape',
+  'size',
+  'type',
+  'variant',
+  'buttonClick'
+], [
+  'buttonClick'
+]);
+
+
+export const ModusWcCard: StencilVueComponent<JSX.ModusWcCard> = /*@__PURE__*/ defineContainer<JSX.ModusWcCard>('modus-wc-card', undefined, [
+  'backgroundFigure',
+  'bordered',
+  'customClass',
+  'layout',
+  'padding'
+]);
+
+
+export const ModusWcCheckbox: StencilVueComponent<JSX.ModusWcCheckbox> = /*@__PURE__*/ defineContainer<JSX.ModusWcCheckbox>('modus-wc-checkbox', undefined, [
+  'customClass',
+  'disabled',
+  'indeterminate',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'name',
+  'required',
+  'size',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcChip: StencilVueComponent<JSX.ModusWcChip> = /*@__PURE__*/ defineContainer<JSX.ModusWcChip>('modus-wc-chip', undefined, [
+  'active',
+  'customClass',
+  'disabled',
+  'hasError',
+  'label',
+  'showRemove',
+  'size',
+  'variant',
+  'chipClick',
+  'chipRemove'
+], [
+  'chipClick',
+  'chipRemove'
+]);
+
+
+export const ModusWcCollapse: StencilVueComponent<JSX.ModusWcCollapse> = /*@__PURE__*/ defineContainer<JSX.ModusWcCollapse>('modus-wc-collapse', undefined, [
+  'bordered',
+  'customClass',
+  'expanded',
+  'collapseId',
+  'options',
+  'expandedChange'
+], [
+  'expandedChange'
+]);
+
+
+export const ModusWcDate: StencilVueComponent<JSX.ModusWcDate> = /*@__PURE__*/ defineContainer<JSX.ModusWcDate>('modus-wc-date', undefined, [
+  'bordered',
+  'customClass',
+  'disabled',
+  'feedback',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'max',
+  'min',
+  'name',
+  'readOnly',
+  'required',
+  'size',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcDivider: StencilVueComponent<JSX.ModusWcDivider> = /*@__PURE__*/ defineContainer<JSX.ModusWcDivider>('modus-wc-divider', undefined, [
+  'color',
+  'content',
+  'customClass',
+  'orientation',
+  'position',
+  'responsive'
+]);
+
+
+export const ModusWcDropdownMenu: StencilVueComponent<JSX.ModusWcDropdownMenu> = /*@__PURE__*/ defineContainer<JSX.ModusWcDropdownMenu>('modus-wc-dropdown-menu', undefined, [
+  'buttonColor',
+  'buttonSize',
+  'buttonVariant',
+  'customClass',
+  'disabled',
+  'menuBordered',
+  'menuOffset',
+  'menuPlacement',
+  'menuSize',
+  'menuVisible',
+  'menuVisibilityChange'
+], [
+  'menuVisibilityChange'
+]);
+
+
+export const ModusWcIcon: StencilVueComponent<JSX.ModusWcIcon> = /*@__PURE__*/ defineContainer<JSX.ModusWcIcon>('modus-wc-icon', undefined, [
+  'customClass',
+  'decorative',
+  'name',
+  'size'
+]);
+
+
+export const ModusWcInputFeedback: StencilVueComponent<JSX.ModusWcInputFeedback> = /*@__PURE__*/ defineContainer<JSX.ModusWcInputFeedback>('modus-wc-input-feedback', undefined, [
+  'customClass',
+  'icon',
+  'level',
+  'message',
+  'size'
+]);
+
+
+export const ModusWcInputLabel: StencilVueComponent<JSX.ModusWcInputLabel> = /*@__PURE__*/ defineContainer<JSX.ModusWcInputLabel>('modus-wc-input-label', undefined, [
+  'forId',
+  'customClass',
+  'labelText',
+  'required',
+  'size',
+  'subLabelText'
+]);
+
+
+export const ModusWcLoader: StencilVueComponent<JSX.ModusWcLoader> = /*@__PURE__*/ defineContainer<JSX.ModusWcLoader>('modus-wc-loader', undefined, [
+  'color',
+  'customClass',
+  'size',
+  'variant'
+]);
+
+
+export const ModusWcMenu: StencilVueComponent<JSX.ModusWcMenu> = /*@__PURE__*/ defineContainer<JSX.ModusWcMenu>('modus-wc-menu', undefined, [
+  'bordered',
+  'customClass',
+  'orientation',
+  'size',
+  'menuFocusout'
+], [
+  'menuFocusout'
+]);
+
+
+export const ModusWcMenuItem: StencilVueComponent<JSX.ModusWcMenuItem> = /*@__PURE__*/ defineContainer<JSX.ModusWcMenuItem>('modus-wc-menu-item', undefined, [
+  'bordered',
+  'customClass',
+  'disabled',
+  'label',
+  'startIcon',
+  'selected',
+  'focused',
+  'size',
+  'subLabel',
+  'value',
+  'itemSelect'
+], [
+  'itemSelect'
+]);
+
+
+export const ModusWcModal: StencilVueComponent<JSX.ModusWcModal> = /*@__PURE__*/ defineContainer<JSX.ModusWcModal>('modus-wc-modal', undefined, [
+  'backdrop',
+  'customClass',
+  'fullscreen',
+  'modalId',
+  'position',
+  'showClose',
+  'showFullscreenToggle'
+]);
+
+
+export const ModusWcNavbar: StencilVueComponent<JSX.ModusWcNavbar> = /*@__PURE__*/ defineContainer<JSX.ModusWcNavbar>('modus-wc-navbar', undefined, [
+  'appsMenuOpen',
+  'condensed',
+  'condensedMenuOpen',
+  'customClass',
+  'mainMenuOpen',
+  'notificationsMenuOpen',
+  'searchDebounceMs',
+  'searchInputOpen',
+  'textOverrides',
+  'userCard',
+  'userMenuOpen',
+  'visibility',
+  'aiClick',
+  'appsClick',
+  'appsMenuOpenChange',
+  'condensedMenuOpenChange',
+  'helpClick',
+  'mainMenuOpenChange',
+  'myTrimbleClick',
+  'notificationsClick',
+  'notificationsMenuOpenChange',
+  'searchChange',
+  'searchClick',
+  'searchInputOpenChange',
+  'signOutClick',
+  'trimbleLogoClick',
+  'userMenuOpenChange'
+], [
+  'aiClick',
+  'appsClick',
+  'appsMenuOpenChange',
+  'condensedMenuOpenChange',
+  'helpClick',
+  'mainMenuOpenChange',
+  'myTrimbleClick',
+  'notificationsClick',
+  'notificationsMenuOpenChange',
+  'searchChange',
+  'searchClick',
+  'searchInputOpenChange',
+  'signOutClick',
+  'trimbleLogoClick',
+  'userMenuOpenChange'
+]);
+
+
+export const ModusWcNumberInput: StencilVueComponent<JSX.ModusWcNumberInput> = /*@__PURE__*/ defineContainer<JSX.ModusWcNumberInput>('modus-wc-number-input', undefined, [
+  'autoComplete',
+  'bordered',
+  'currencySymbol',
+  'customClass',
+  'disabled',
+  'feedback',
+  'inputId',
+  'inputMode',
+  'inputTabIndex',
+  'label',
+  'max',
+  'min',
+  'name',
+  'placeholder',
+  'readOnly',
+  'required',
+  'size',
+  'step',
+  'type',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcPagination: StencilVueComponent<JSX.ModusWcPagination> = /*@__PURE__*/ defineContainer<JSX.ModusWcPagination>('modus-wc-pagination', undefined, [
+  'ariaLabelValues',
+  'count',
+  'customClass',
+  'nextButtonText',
+  'page',
+  'prevButtonText',
+  'size',
+  'pageChange'
+], [
+  'pageChange'
+]);
+
+
+export const ModusWcProgress: StencilVueComponent<JSX.ModusWcProgress> = /*@__PURE__*/ defineContainer<JSX.ModusWcProgress>('modus-wc-progress', undefined, [
+  'customClass',
+  'indeterminate',
+  'label',
+  'max',
+  'value',
+  'variant'
+]);
+
+
+export const ModusWcRadio: StencilVueComponent<JSX.ModusWcRadio> = /*@__PURE__*/ defineContainer<JSX.ModusWcRadio>('modus-wc-radio', undefined, [
+  'customClass',
+  'disabled',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'name',
+  'required',
+  'size',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcRating: StencilVueComponent<JSX.ModusWcRating> = /*@__PURE__*/ defineContainer<JSX.ModusWcRating>('modus-wc-rating', undefined, [
+  'allowHalf',
+  'count',
+  'customClass',
+  'disabled',
+  'getAriaLabelText',
+  'size',
+  'variant',
+  'value',
+  'ratingChange'
+], [
+  'ratingChange'
+]);
+
+
+export const ModusWcSelect: StencilVueComponent<JSX.ModusWcSelect> = /*@__PURE__*/ defineContainer<JSX.ModusWcSelect>('modus-wc-select', undefined, [
+  'bordered',
+  'customClass',
+  'disabled',
+  'feedback',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'name',
+  'options',
+  'required',
+  'size',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcSideNavigation: StencilVueComponent<JSX.ModusWcSideNavigation> = /*@__PURE__*/ defineContainer<JSX.ModusWcSideNavigation>('modus-wc-side-navigation', undefined, [
+  'collapseOnClickOutside',
+  'customClass',
+  'expanded',
+  'maxWidth',
+  'mode',
+  'targetContent',
+  'expandedChange'
+], [
+  'expandedChange'
+]);
+
+
+export const ModusWcSkeleton: StencilVueComponent<JSX.ModusWcSkeleton> = /*@__PURE__*/ defineContainer<JSX.ModusWcSkeleton>('modus-wc-skeleton', undefined, [
+  'customClass',
+  'height',
+  'shape',
+  'width'
+]);
+
+
+export const ModusWcSlider: StencilVueComponent<JSX.ModusWcSlider> = /*@__PURE__*/ defineContainer<JSX.ModusWcSlider>('modus-wc-slider', undefined, [
+  'customClass',
+  'disabled',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'max',
+  'min',
+  'name',
+  'required',
+  'size',
+  'step',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcStepper: StencilVueComponent<JSX.ModusWcStepper> = /*@__PURE__*/ defineContainer<JSX.ModusWcStepper>('modus-wc-stepper', undefined, [
+  'customClass',
+  'orientation',
+  'steps'
+]);
+
+
+export const ModusWcSwitch: StencilVueComponent<JSX.ModusWcSwitch> = /*@__PURE__*/ defineContainer<JSX.ModusWcSwitch>('modus-wc-switch', undefined, [
+  'customClass',
+  'disabled',
+  'indeterminate',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'name',
+  'required',
+  'size',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcTable: StencilVueComponent<JSX.ModusWcTable> = /*@__PURE__*/ defineContainer<JSX.ModusWcTable>('modus-wc-table', undefined, [
+  'editable',
+  'columns',
+  'customClass',
+  'data',
+  'density',
+  'hover',
+  'currentPage',
+  'paginated',
+  'pageSizeOptions',
+  'showPageSizeSelector',
+  'sortable',
+  'selectable',
+  'selectedRowIds',
+  'zebra',
+  'cellEditStart',
+  'cellEditCommit',
+  'rowClick',
+  'sortChange',
+  'paginationChange',
+  'rowSelectionChange'
+], [
+  'cellEditStart',
+  'cellEditCommit',
+  'rowClick',
+  'sortChange',
+  'paginationChange',
+  'rowSelectionChange'
+]);
+
+
+export const ModusWcTabs: StencilVueComponent<JSX.ModusWcTabs> = /*@__PURE__*/ defineContainer<JSX.ModusWcTabs>('modus-wc-tabs', undefined, [
+  'activeTabIndex',
+  'customClass',
+  'size',
+  'tabs',
+  'tabStyle',
+  'tabChange'
+], [
+  'tabChange'
+]);
+
+
+export const ModusWcTextInput: StencilVueComponent<JSX.ModusWcTextInput> = /*@__PURE__*/ defineContainer<JSX.ModusWcTextInput>('modus-wc-text-input', undefined, [
+  'autoCapitalize',
+  'autoComplete',
+  'autoCorrect',
+  'bordered',
+  'clearAriaLabel',
+  'customClass',
+  'disabled',
+  'enterkeyhint',
+  'feedback',
+  'includeClear',
+  'includeSearch',
+  'inputId',
+  'inputMode',
+  'inputTabIndex',
+  'label',
+  'maxLength',
+  'minLength',
+  'name',
+  'pattern',
+  'placeholder',
+  'readOnly',
+  'required',
+  'size',
+  'type',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcTextarea: StencilVueComponent<JSX.ModusWcTextarea> = /*@__PURE__*/ defineContainer<JSX.ModusWcTextarea>('modus-wc-textarea', undefined, [
+  'autoCorrect',
+  'bordered',
+  'customClass',
+  'disabled',
+  'enterkeyhint',
+  'feedback',
+  'inputId',
+  'inputTabIndex',
+  'label',
+  'maxLength',
+  'name',
+  'placeholder',
+  'readonly',
+  'required',
+  'rows',
+  'size',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcThemeProvider: StencilVueComponent<JSX.ModusWcThemeProvider> = /*@__PURE__*/ defineContainer<JSX.ModusWcThemeProvider>('modus-wc-theme-provider', undefined, [
+  'initialTheme'
+]);
+
+
+export const ModusWcThemeSwitcher: StencilVueComponent<JSX.ModusWcThemeSwitcher> = /*@__PURE__*/ defineContainer<JSX.ModusWcThemeSwitcher>('modus-wc-theme-switcher', undefined, [
+  'customClass',
+  'themeChange'
+], [
+  'themeChange'
+]);
+
+
+export const ModusWcTimeInput: StencilVueComponent<JSX.ModusWcTimeInput> = /*@__PURE__*/ defineContainer<JSX.ModusWcTimeInput>('modus-wc-time-input', undefined, [
+  'autoComplete',
+  'bordered',
+  'customClass',
+  'datalistOptions',
+  'disabled',
+  'feedback',
+  'inputId',
+  'inputTabIndex',
+  'datalistId',
+  'label',
+  'max',
+  'min',
+  'name',
+  'readOnly',
+  'required',
+  'showSeconds',
+  'size',
+  'step',
+  'value',
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+], [
+  'inputBlur',
+  'inputChange',
+  'inputFocus'
+]);
+
+
+export const ModusWcToast: StencilVueComponent<JSX.ModusWcToast> = /*@__PURE__*/ defineContainer<JSX.ModusWcToast>('modus-wc-toast', undefined, [
+  'customClass',
+  'delay',
+  'position'
+]);
+
+
+export const ModusWcToolbar: StencilVueComponent<JSX.ModusWcToolbar> = /*@__PURE__*/ defineContainer<JSX.ModusWcToolbar>('modus-wc-toolbar', undefined, [
+  'customClass'
+]);
+
+
+export const ModusWcTooltip: StencilVueComponent<JSX.ModusWcTooltip> = /*@__PURE__*/ defineContainer<JSX.ModusWcTooltip>('modus-wc-tooltip', undefined, [
+  'content',
+  'customClass',
+  'disabled',
+  'forceOpen',
+  'tooltipId',
+  'position'
+]);
+
+
+export const ModusWcTypography: StencilVueComponent<JSX.ModusWcTypography> = /*@__PURE__*/ defineContainer<JSX.ModusWcTypography>('modus-wc-typography', undefined, [
+  'customClass',
+  'size',
+  'variant',
+  'weight'
+]);
+
