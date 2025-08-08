c30f917bf4793d8ff3d4d56165cca4b0f27f8733
Gabe Piltzer <gabriel_piltzer@trimble.com>
2025-08-08 13:33:00 -0700

    feat: add delay to alert

--- src/components.d.ts
+++ src/components.d.ts
@@ -65,6 +65,10 @@ export namespace Components {
          */
         "customClass"?: string;
         /**
+          * Time taken to dismiss the alert in milliseconds
+         */
+        "delay"?: number;
+        /**
           * Whether the alert has a dismiss button
          */
         "dismissible"?: boolean;
@@ -2731,6 +2735,10 @@ declare namespace LocalJSX {
           * Custom CSS class to apply to the outer div element.
          */
         "customClass"?: string;
+        /**
+          * Time taken to dismiss the alert in milliseconds
+         */
+        "delay"?: number;
         /**
           * Whether the alert has a dismiss button
          */
