bdf5bff1968a7b6d29ea30d9a3046d3a22fb5642
ElishaSamPeterPrabhu <ElishaSamPeterPrabhu@users.noreply.github.com>
2025-08-18 08:59:04 +0000

    Deploy preview for PR 528 🛫

---
+++ pr-preview/pr-528/assets/accessibility-C3XhjM_v.js
@@ -0,0 +1,22 @@
+import{j as e,M as o}from"./index-BJ_1Gp15.js";import{useMDXComponents as n}from"./index-_I-BtN6Q.js";import"./iframe-CAgzIwps.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function t(i){const s={a:"a",h1:"h1",h2:"h2",h3:"h3",li:"li",p:"p",ul:"ul",...n(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Documentation/Accessibility"}),`
+`,e.jsx(s.h1,{id:"accessibility",children:"Accessibility"}),`
+`,e.jsxs(s.p,{children:["The Modus Web Components package is designed to be accessible by default. This means that the components are built with accessibility in mind and are tested to ensure they meet the ",e.jsx(s.a,{href:"https://www.w3.org/WAI/standards-guidelines/wcag/",rel:"nofollow",children:"Web Content Accessibility Guidelines (WCAG)"}),"."]}),`
+`,e.jsx(s.p,{children:"Features:"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsxs(s.li,{children:["Meet ",e.jsx(s.a,{href:"https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html",rel:"nofollow",children:"minimum contrast requirements (Level AA)"}),"."]}),`
+`,e.jsx(s.li,{children:"Keyboard navigable where applicable."}),`
+`,e.jsx(s.li,{children:"Screen reader accessible."}),`
+`,e.jsxs(s.li,{children:["Supports the ",e.jsx(s.a,{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion",rel:"nofollow",children:"prefers-reduced-motion media query"})," to minimize the amount of non-essential motion."]}),`
+`]}),`
+`,e.jsxs(s.p,{children:["Found an accessibility issue? Please ",e.jsx(s.a,{href:"https://github.com/trimble-oss/modus-web-components/issues/new?assignees=&labels=accessibility&template=accessibility-issue-report.md&title=",rel:"nofollow",children:"report it at GitHub"}),"."]}),`
+`,e.jsx(s.h2,{id:"for-contributors",children:"For contributors"}),`
+`,e.jsx(s.p,{children:"The Storybook site has an Accessibility tab that can be used to test the components for accessibility issues. If you are contributing to the Modus Web Components package please ensure that your changes do not introduce accessibility issues. If there are any violations please address them before submitting your changes. If you are unsure, please reach out to the team for guidance."}),`
+`,e.jsx(s.h2,{id:"useful-tools-for-testing-your-sites",children:"Useful tools for testing your sites"}),`
+`,e.jsx(s.h3,{id:"browser-extensions",children:"Browser extensions"}),`
+`,e.jsxs(s.ul,{children:[`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://chromewebstore.google.com/detail/axe-devtools-web-accessib/lhdoppojpmngadmnindnejefpokejbdd",rel:"nofollow",children:"axe DevTools"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://chromewebstore.google.com/detail/lighthouse/blipmdconlkpinefehnmjammfjpmpbjk",rel:"nofollow",children:"Lighthouse"})}),`
+`,e.jsx(s.li,{children:e.jsx(s.a,{href:"https://accessibilityinsights.io/downloads/",rel:"nofollow",children:"Microsoft Accessibility Insights"})}),`
+`]}),`
+`,e.jsx(s.h2,{id:"further-reading",children:"Further Reading"}),`
+`,e.jsxs(s.p,{children:["You can find more information on accessibility on the ",e.jsx(s.a,{href:"https://modus.trimble.com/foundations/accessibility/",rel:"nofollow",children:"Modus website"}),"."]})]})}function u(i={}){const{wrapper:s}={...n(),...i.components};return s?e.jsx(s,{...i,children:e.jsx(t,{...i})}):t(i)}export{u as default};
