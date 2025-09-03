52a75fb8f9f5d4fe85ad4faaa2dd99d911874e19
Mitch Ray <mitch_ray@trimble.com>
2025-09-02 23:25:19 -0600

    added connect theme to system (#554)
    Co-authored-by: Kavin <152589235+Kavin-Trimble@users.noreply.github.com>
    Co-authored-by: Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>

--- src/providers/theme/theme.types.ts
+++ src/providers/theme/theme.types.ts
@@ -1,7 +1,7 @@
 /* istanbul ignore file - this may not make it to production */
 
 export type ThemeMode = 'light' | 'dark';
-export type ThemeName = 'modus-classic' | 'modus-modern';
+export type ThemeName = 'modus-classic' | 'modus-modern' | 'connect';
 
 export interface IThemeConfig {
   mode: ThemeMode;
