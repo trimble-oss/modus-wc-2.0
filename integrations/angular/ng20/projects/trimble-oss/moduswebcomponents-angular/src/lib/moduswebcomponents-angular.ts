5330d377ca65581f4ddbb2fc64cf57d4d504b464
Alan Thomas <alan_thomas@trimble.com>
2025-09-11 13:37:43 -0600

    step 2

---
+++ integrations/angular/ng20/projects/trimble-oss/moduswebcomponents-angular/src/lib/moduswebcomponents-angular.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-moduswebcomponents-angular',
+  imports: [],
+  template: `
+    <p>
+      moduswebcomponents-angular works!
+    </p>
+  `,
+  styles: ``
+})
+export class ModuswebcomponentsAngular {
+
+}
---
+++ integrations/angular/ng20/projects/trimble-oss/moduswebcomponents-angular/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of moduswebcomponents-angular
+ */
+
+export * from './lib/moduswebcomponents-angular';
