904ceb8fc395fc7c158cb3b2468e0b865d8047e5
Kavin <152589235+Kavin-Trimble@users.noreply.github.com>
2025-09-30 19:00:05 +0530

    update testing comments

--- src/components/modus-wc-autocomplete/modus-wc-autocomplete-core.tsx
+++ src/components/modus-wc-autocomplete/modus-wc-autocomplete-core.tsx
@@ -601,7 +601,7 @@ export function renderClearButton(
     <modus-wc-button
       onClick={params.onClearAll}
       variant="borderless"
-      color="tertiary"
+      custom-class="modus-wc-autocomplete-clear-button"
       aria-label="Clear all"
       disabled={params.disabled || params.readOnly}
       size="xs"
@@ -643,7 +643,6 @@ export function renderExpandCollapseButton(
       custom-class={`modus-wc-autocomplete-expand-button ${params.isChipsExpanded ? 'expanded' : ''}`}
       onClick={params.onToggleExpansion}
       variant="borderless"
-      color="tertiary"
       aria-label={
         params.isChipsExpanded
           ? 'Collapse chips'
