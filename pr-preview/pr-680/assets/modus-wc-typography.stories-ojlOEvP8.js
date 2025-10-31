23bb2a4ce473bb035ff0b17b7eb80a7bf24e8d89
jewel-shajan <jewel-shajan@users.noreply.github.com>
2025-10-31 13:44:31 +0000

    Deploy preview for PR 680 🛫

---
+++ pr-preview/pr-680/assets/modus-wc-typography.stories-ojlOEvP8.js
@@ -0,0 +1,45 @@
+import{x as O,B as A}from"./lit-element-C8zulti1.js";import{o as d}from"./if-defined-yv6owfG8.js";const F="The quick brown fox jumps over the lazy dog",K={title:"Components/Typography",component:"modus-wc-typography",args:{hierarchy:"p",size:"md",weight:"normal"},argTypes:{hierarchy:{control:{type:"select"},options:["h1","h2","h3","h4","h5","h6","p"]},size:{control:{type:"select"},options:["xs","sm","md","lg","xl","2xl","3xl","4xl","5xl","6xl","7xl","8xl","9xl"]},weight:{control:{type:"select"},options:["light","normal","semibold","bold"]}},decorators:[r=>{const i=document.createElement("div"),l=document.createElement("template");return l.innerHTML=F,(()=>{A(r(),i);const m=i.querySelector("modus-wc-typography");m&&!m.textContent&&(m.textContent=l.innerHTML)})(),i}]},e={render:r=>O`
+<modus-wc-typography
+  custom-class=${d(r["custom-class"])}
+  hierarchy=${r.hierarchy}
+  size=${d(r.size)}
+  weight=${d(r.weight)}>
+</modus-wc-typography>
+   `},a={...e},s={...e,args:{hierarchy:"h1"}},o={...e,args:{hierarchy:"h2"}},t={...e,args:{hierarchy:"h3"}},n={...e,args:{hierarchy:"h4"}},c={...e,args:{hierarchy:"h5"}},p={...e,args:{hierarchy:"h6"}},h={...e,args:{hierarchy:"p"}};var g,u,y;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  ...Template
+}`,...(y=(u=a.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var x,H,T;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h1'
+  }
+}`,...(T=(H=s.parameters)==null?void 0:H.docs)==null?void 0:T.source}}};var S,w,f;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h2'
+  }
+}`,...(f=(w=o.parameters)==null?void 0:w.docs)==null?void 0:f.source}}};var z,$,b;t.parameters={...t.parameters,docs:{...(z=t.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h3'
+  }
+}`,...(b=($=t.parameters)==null?void 0:$.docs)==null?void 0:b.source}}};var C,E,q;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h4'
+  }
+}`,...(q=(E=n.parameters)==null?void 0:E.docs)==null?void 0:q.source}}};var v,D,L;c.parameters={...c.parameters,docs:{...(v=c.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h5'
+  }
+}`,...(L=(D=c.parameters)==null?void 0:D.docs)==null?void 0:L.source}}};var M,P,_;p.parameters={...p.parameters,docs:{...(M=p.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h6'
+  }
+}`,...(_=(P=p.parameters)==null?void 0:P.docs)==null?void 0:_.source}}};var j,k,B;h.parameters={...h.parameters,docs:{...(j=h.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'p'
+  }
+}`,...(B=(k=h.parameters)==null?void 0:k.docs)==null?void 0:B.source}}};const N=["Default","Heading1","Heading2","Heading3","Heading4","Heading5","Heading6","Paragraph"];export{a as Default,s as Heading1,o as Heading2,t as Heading3,n as Heading4,c as Heading5,p as Heading6,h as Paragraph,N as __namedExportsOrder,K as default};
