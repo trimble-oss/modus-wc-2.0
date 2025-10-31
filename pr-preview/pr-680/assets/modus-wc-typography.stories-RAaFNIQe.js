f8b39160d2531afccd50259b83ccbd0693ddd5fa
jewel-shajan <jewel-shajan@users.noreply.github.com>
2025-10-31 09:17:51 +0000

    Deploy preview for PR 680 🛫

---
+++ pr-preview/pr-680/assets/modus-wc-typography.stories-RAaFNIQe.js
@@ -0,0 +1,150 @@
+import{x as s,B as O}from"./lit-element-C8zulti1.js";import{o as r}from"./if-defined-yv6owfG8.js";const A="The quick brown fox jumps over the lazy dog",J={title:"Components/Typography",component:"modus-wc-typography",args:{hierarchy:"p",size:"md",weight:"normal"},argTypes:{hierarchy:{control:{type:"select"},options:["h1","h2","h3","h4","h5","h6","p"]},size:{control:{type:"select"},options:["xs","sm","md","lg","xl","2xl","3xl","4xl","5xl","6xl","7xl","8xl","9xl"]},weight:{control:{type:"select"},options:["light","normal","semibold","bold"]}},decorators:[e=>{const g=document.createElement("div"),p=document.createElement("template");return p.innerHTML=A,(()=>{O(e(),g);const m=g.querySelector("modus-wc-typography");m&&!m.textContent&&(m.textContent=p.innerHTML)})(),g}]},a={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `},c={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"h1"}},o={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"h2"}},t={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"h3"}},i={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"h4"}},n={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"h5"}},h={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"h6"}},y={render:e=>s`
+    <modus-wc-typography
+      custom-class=${r(e["custom-class"])}
+      hierarchy=${e.hierarchy}
+      size=${r(e.size)}
+      weight=${r(e.weight)}
+    ></modus-wc-typography>
+  `,args:{hierarchy:"p"}};var d,u,l;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`
+}`,...(l=(u=a.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var w,$,f;c.parameters={...c.parameters,docs:{...(w=c.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'h1'
+  }
+}`,...(f=($=c.parameters)==null?void 0:$.docs)==null?void 0:f.source}}};var z,D,x;o.parameters={...o.parameters,docs:{...(z=o.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'h2'
+  }
+}`,...(x=(D=o.parameters)==null?void 0:D.docs)==null?void 0:x.source}}};var H,S,T;t.parameters={...t.parameters,docs:{...(H=t.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'h3'
+  }
+}`,...(T=(S=t.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var b,C,E;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'h4'
+  }
+}`,...(E=(C=i.parameters)==null?void 0:C.docs)==null?void 0:E.source}}};var q,v,L;n.parameters={...n.parameters,docs:{...(q=n.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'h5'
+  }
+}`,...(L=(v=n.parameters)==null?void 0:v.docs)==null?void 0:L.source}}};var M,P,_;h.parameters={...h.parameters,docs:{...(M=h.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'h6'
+  }
+}`,...(_=(P=h.parameters)==null?void 0:P.docs)==null?void 0:_.source}}};var j,k,B;y.parameters={...y.parameters,docs:{...(j=y.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: args => html\`
+    <modus-wc-typography
+      custom-class=\${ifDefined(args['custom-class'])}
+      hierarchy=\${args.hierarchy}
+      size=\${ifDefined(args.size)}
+      weight=\${ifDefined(args.weight)}
+    ></modus-wc-typography>
+  \`,
+  args: {
+    hierarchy: 'p'
+  }
+}`,...(B=(k=y.parameters)==null?void 0:k.docs)==null?void 0:B.source}}};const K=["Default","Heading1","Heading2","Heading3","Heading4","Heading5","Heading6","Paragraph"];export{a as Default,c as Heading1,o as Heading2,t as Heading3,i as Heading4,n as Heading5,h as Heading6,y as Paragraph,K as __namedExportsOrder,J as default};
