7f0f07205d18937180d1999da2515e0241c2bf30
prashanthr6383 <prashanthr6383@users.noreply.github.com>
2025-12-22 07:40:53 +0000

    Deploy preview for PR 757 🛫

---
+++ pr-preview/pr-757/assets/modus-wc-typography.stories-C5-c09SN.js
@@ -0,0 +1,52 @@
+import{x as B}from"./lit-element-C8zulti1.js";import{o as m}from"./if-defined-yv6owfG8.js";const F="The quick brown fox jumps over the lazy dog",J={title:"Components/Typography",component:"modus-wc-typography",args:{hierarchy:"p",label:F,size:"md",weight:"normal"},argTypes:{hierarchy:{control:{type:"select"},options:["h1","h2","h3","h4","h5","h6","p"]},size:{control:{type:"select"},options:["xs","sm","md","lg","xl","2xl","3xl","4xl","5xl","6xl","7xl","8xl","9xl"]},weight:{control:{type:"select"},options:["light","normal","semibold","bold"]}}},e={render:r=>B`
+<modus-wc-typography
+  custom-class=${m(r["custom-class"])}
+  hierarchy=${r.hierarchy}
+  label=${r.label}
+  size=${m(r.size)}
+  weight=${m(r.weight)}
+  ></modus-wc-typography>
+   `},a={...e},s={...e,args:{hierarchy:"h1"}},o={...e,args:{hierarchy:"h2"}},t={...e,args:{hierarchy:"h3"}},c={...e,args:{hierarchy:"h4"}},n={...e,args:{hierarchy:"h5"}},p={...e,args:{hierarchy:"h6"}},h={...e,args:{hierarchy:"p"}},i={...e,args:{hierarchy:"p",label:"This text is set via the label prop"}};var l,d,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  ...Template
+}`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,y,x;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h1'
+  }
+}`,...(x=(y=s.parameters)==null?void 0:y.docs)==null?void 0:x.source}}};var T,b,H;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h2'
+  }
+}`,...(H=(b=o.parameters)==null?void 0:b.docs)==null?void 0:H.source}}};var S,w,f;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h3'
+  }
+}`,...(f=(w=t.parameters)==null?void 0:w.docs)==null?void 0:f.source}}};var z,$,v;c.parameters={...c.parameters,docs:{...(z=c.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h4'
+  }
+}`,...(v=($=c.parameters)==null?void 0:$.docs)==null?void 0:v.source}}};var D,L,P;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h5'
+  }
+}`,...(P=(L=n.parameters)==null?void 0:L.docs)==null?void 0:P.source}}};var W,_,j;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'h6'
+  }
+}`,...(j=(_=p.parameters)==null?void 0:_.docs)==null?void 0:j.source}}};var k,q,C;h.parameters={...h.parameters,docs:{...(k=h.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'p'
+  }
+}`,...(C=(q=h.parameters)==null?void 0:q.docs)==null?void 0:C.source}}};var E,O,A;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  ...Template,
+  args: {
+    hierarchy: 'p',
+    label: 'This text is set via the label prop'
+  }
+}`,...(A=(O=i.parameters)==null?void 0:O.docs)==null?void 0:A.source}}};const K=["Default","Heading1","Heading2","Heading3","Heading4","Heading5","Heading6","Paragraph","WithLabel"];export{a as Default,s as Heading1,o as Heading2,t as Heading3,c as Heading4,n as Heading5,p as Heading6,h as Paragraph,i as WithLabel,K as __namedExportsOrder,J as default};
