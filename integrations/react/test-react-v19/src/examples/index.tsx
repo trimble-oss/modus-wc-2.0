b915f460fc7d9c7838aa022361585d3ee232259b
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2026-01-30 15:44:09 +0530

    logo do

--- integrations/react/test-react-v19/src/examples/index.tsx
+++ integrations/react/test-react-v19/src/examples/index.tsx
@@ -1,3 +1,4 @@
 export { default as Badge } from "./ModusBadgeExamples";
 export { default as Icon } from "./ModusIconExamples";
 export { default as Tabs } from "./ModusTabsExamples";
+export { default as Modal } from "./ModusModalExamples";
