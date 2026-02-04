76082e67e65c15ef4d7810780ef82edcac0d906c
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2026-02-04 09:50:23 +0530

    update test app

--- integrations/vue/test-app/src/main.ts
+++ integrations/vue/test-app/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { ComponentLibrary } from './plugin'
+import { ComponentLibrary } from '@trimble-oss/moduswebcomponents-vue'
 import '@trimble-oss/moduswebcomponents/modus-wc-styles.css'
 
 createApp(App).use(ComponentLibrary).mount('#app') 
