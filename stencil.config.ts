4e064151b688920728dcf65749fe51fe10b27159
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2026-02-24 12:57:46 +0530

    variables only

--- stencil.config.ts
+++ stencil.config.ts
@@ -42,6 +42,10 @@ export const config: Config = {
       copy: [
         // This is scoped to /src
         { src: './styles/output.css', dest: 'dist/modus-wc-styles.css' },
+        {
+          src: './styles/modus-wc-variables.css',
+          dest: 'dist/modus-wc-variables.css',
+        },
         { src: './styles/modus-icons.css', dest: 'dist/modus-icons.css' },
         { src: './styles/assets', dest: 'dist/styles/assets' },
         { src: './styles/assets', dest: 'dist/assets' },
