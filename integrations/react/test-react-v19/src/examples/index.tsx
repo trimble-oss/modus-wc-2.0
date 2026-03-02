98e5484985d6c30889ad3fbfb9571223ed7f06ca
Elisha Sam Peter Prabhu <122255626+ElishaSamPeterPrabhu@users.noreply.github.com>
2026-03-02 12:02:51 +0530

    Fix: Date input validation (#850)
    Co-authored-by: Jewel Shajan <jewel_shajan@trimble.com>

--- integrations/react/test-react-v19/src/examples/index.tsx
+++ integrations/react/test-react-v19/src/examples/index.tsx
@@ -1,3 +1,4 @@
 export { default as Badge } from "./ModusBadgeExamples";
+export { default as Date } from "./ModusDateExamples";
 export { default as Icon } from "./ModusIconExamples";
 export { default as Tabs } from "./ModusTabsExamples";
