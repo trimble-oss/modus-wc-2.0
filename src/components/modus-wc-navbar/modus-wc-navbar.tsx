e02abebd35af031acaa4bed46de8a47ecd6bdfa9
jewel-shajan <jewel_shajan@trimble.com>
2026-03-04 10:02:59 +0530

    Navbar: Reverting event name from logoClick to trimbleLogoClick

--- src/components/modus-wc-navbar/modus-wc-navbar.tsx
+++ src/components/modus-wc-navbar/modus-wc-navbar.tsx
@@ -73,13 +73,6 @@ export interface INavbarUserCard {
  * A customizable navbar component used for top level navigation of all Trimble applications.
  *
  *The component supports a 'main-menu', 'notifications', and 'apps' <slot> for injecting custom HTML menus. It also supports a 'start', 'center', and 'end' `<slot>` for injecting additional custom HTML.
-
-<strong><span style="color: black">⚠️ Deprecation Alert</span></strong>
-
- 
-The `trimbleLogoClick` event is deprecated and will be removed in a future major version.
-Please use the `logoClick` event instead, which serves the same purpose and is not tied to a specific logo name.
-The `logoClick` event will be emitted whenever the logo is clicked, regardless of the `logoName` prop value.
  */
 @Component({
   tag: 'modus-wc-navbar',
@@ -186,10 +179,6 @@ export class ModusWcNavbar {
   @StencilEvent() signOutClick!: EventEmitter<MouseEvent | KeyboardEvent>;
 
   /** Event emitted when the logo is clicked or activated via keyboard. */
-  @StencilEvent() logoClick!: EventEmitter<MouseEvent | KeyboardEvent>;
-
-  /** @deprecated */
-  /** Deprecated: Use logoClick instead. This event will be removed in a future release.*/
   @StencilEvent() trimbleLogoClick!: EventEmitter<MouseEvent | KeyboardEvent>;
 
   /** Event emitted when the user menu open state changes. */
@@ -374,10 +363,9 @@ export class ModusWcNavbar {
     this.signOutClick.emit(event.detail);
   };
 
-  private handleLogoClick = (
+  private handleTrimbleLogoClick = (
     event: CustomEvent<MouseEvent | KeyboardEvent>
   ) => {
-    this.logoClick.emit(event.detail);
     this.trimbleLogoClick.emit(event.detail);
   };
 
@@ -461,7 +449,7 @@ export class ModusWcNavbar {
             <modus-wc-button
               aria-label={`${accessibleName} logo`}
               customClass="logo"
-              onButtonClick={this.handleLogoClick}
+              onButtonClick={this.handleTrimbleLogoClick}
               size="sm"
               variant="borderless"
             >
