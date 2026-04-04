9ef92079ecbcc46b3a004574c391497e68da92ec
Ryan Niebur <ryan_niebur@trimble.com>
2026-04-03 17:21:47 -0700

    Fix prettier on stencil.config.ts

--- stencil.config.ts
+++ stencil.config.ts
@@ -104,7 +104,8 @@ export const config: Config = {
       customElementsDir: 'components',
     }),
     blazorOutputTarget({
-      outDir: './integrations/blazor/stencil-generated/ModusWebComponents.Blazor',
+      outDir:
+        './integrations/blazor/stencil-generated/ModusWebComponents.Blazor',
       packageName: 'ModusWebComponents.Blazor',
       namespace: 'ModusWebComponents.Blazor',
     }),
