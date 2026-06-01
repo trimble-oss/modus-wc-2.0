1e263cc714b26128f9bbf9448f4da3ece090a95d
jewel-shajan <jewel_shajan@trimble.com>
2026-06-01 11:54:46 +0530

    fix prettier

--- src/components/modus-wc-alert/modus-wc-alert.tsx
+++ src/components/modus-wc-alert/modus-wc-alert.tsx
@@ -52,7 +52,8 @@ export class ModusWcAlert {
   @Prop() icon?: string;
 
   /** The variant of the alert. */
-  @Prop() variant?: 'default' | 'error' | 'info' | 'success' | 'warning' = 'info';
+  @Prop() variant?: 'default' | 'error' | 'info' | 'success' | 'warning' =
+    'info';
 
   /** An event that fires when the alert is dismissed */
   @Event() dismissClick!: EventEmitter;
