96d8e54aac1e9ec5dabd4c51fe2b7bca457d6775
Kavin <152589235+Kavin-Trimble@users.noreply.github.com>
2026-09-10 16:58:34 +0530

    Fix aria attribute for the input components

--- src/components/modus-wc-radio/modus-wc-radio.stories.ts
+++ src/components/modus-wc-radio/modus-wc-radio.stories.ts
@@ -51,7 +51,6 @@ export const Default: Story = {
   render: (args) => {
     return html`
       <modus-wc-radio
-        aria-label="Radio"
         custom-class=${ifDefined(args['custom-class'])}
         ?disabled=${args.disabled}
         input-id=${ifDefined(args['input-id'])}
